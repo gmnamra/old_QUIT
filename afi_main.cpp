b0336e0008475e769597365a9fbe131f8547562e
Tobias Wood <tobias.wood@kcl.ac.uk>
2012-12-18 14:46:28 +0000

    Updated AFI and Phasemap to use librecon.

--- afi_main.cpp
+++ afi_main.cpp
@@ -14,7 +14,14 @@
 #include "NiftiImage.h"
 #include "procpar.h"
 
-const std::string usage("Usage is: afi [options] input output \n\
+using namespace std;
+#define USE_PROCPAR
+#ifdef USE_PROCPAR
+	#include "procpar.h"
+	using namespace Recon;
+#endif
+
+const string usage("Usage is: afi [options] input output \n\
 \
 Options:\n\
 	--mask, -m file  : Mask input with specified file.\n\
@@ -36,8 +43,7 @@ int main(int argc, char **argv)
 	};
 	
 	int indexptr = 0, c;
-	std::string procPath, outPrefix;
-	par_t *pars;
+	string procPath, outPrefix;
 	double n, nomFlip;
 	double *tr1, *tr2, *flip, *B1, *mask = NULL;
 	NiftiImage inFile;
@@ -46,43 +52,42 @@ int main(int argc, char **argv)
 		switch (c)
 		{
 			case 'm':
-				std::cout << "Reading mask." << std::endl;
+				cout << "Reading mask." << endl;
 				inFile.open(optarg, NiftiImage::NIFTI_READ);
 				mask = inFile.readVolume<double>(0);
 				inFile.close();
 				break;
 		}
 	}
-	if ((argc - optind) != 2)
-	{
-		std::cout << usage << std::endl;
+	if ((argc - optind) != 2) {
+		cout << usage << endl;
 		exit(EXIT_FAILURE);
 	}
-	std::cout << "Opening input file " << argv[optind] << std::endl;
+	cout << "Opening input file " << argv[optind] << endl;
 	inFile.open(argv[optind], NiftiImage::NIFTI_READ);
-	procPath = inFile.basename() + ".procpar";
-	if ((pars = readProcpar(procPath.c_str())))
-	{
+	#ifdef USE_PROCPAR
+	ParameterList pars;
+	if (ReadProcpar(inFile.basename() + ".procpar", pars)) {
 		// From Sam Hurley. The sequence is implemented by waiting afi_dummy
 		// periods after the first afi_tr.
-		n = realVal(pars, "afi_dummy", 0) + 1;
-		nomFlip = realVal(pars, "flip1", 0);
-		fprintf(stdout, "Read TR2/TR1 ratio of %f and flip-angle %f degrees from procpar.\n", n, nomFlip);
-	}
-	else
+		n = RealValue(pars, "afi_dummy") + 1;
+		nomFlip = RealValue(pars, "flip1");
+		cout << "Read TR2/TR1 ratio of " << n << " and flip-angle " << nomFlip << " degrees from procpar." << endl;
+	} else
+	#endif
 	{
-		fprintf(stdout, "Enter TR2/TR1 (ratio) and flip-angle (degrees): ");
-		fscanf(stdin, "%lf %lf", &n, &nomFlip);
+		cout << "Enter TR2/TR1 (ratio) and flip-angle (degrees): ";
+		cin >> n >> nomFlip;
 	}
 	nomFlip = nomFlip * M_PI / 180.;
 	tr1 = inFile.readVolume<double>(0);
 	tr2 = inFile.readVolume<double>(1);
 	inFile.close();
-	outPrefix = std::string(argv[++optind]);
+	outPrefix = string(argv[++optind]);
 	flip = (double *)malloc(inFile.voxelsPerVolume() * sizeof(double));
 	B1   = (double *)malloc(inFile.voxelsPerVolume() * sizeof(double));
-	std::cout << "Allocated output memory." << std::endl;
-	std::cout << "Processing..." << std::endl;
+	cout << "Allocated output memory." << endl;
+	cout << "Processing..." << endl;
 	for (size_t vox = 0; vox < inFile.voxelsPerVolume(); vox++) {
 		if (!mask || mask[vox] > 0.) {
 			double r = tr2[vox] / tr1[vox];
@@ -99,8 +104,8 @@ int main(int argc, char **argv)
 			B1[vox] = 1.; // So smoothing doesn't get messed up
 	}
 	NiftiImage outFile = inFile; // Could re-use infile, this is marginally clearer
-	std::string outPath = outPrefix + "_flip.nii.gz";
-	std::cout << "Writing actual flip angle to " << outPath << "..." << std::endl;
+	string outPath = outPrefix + "_flip.nii.gz";
+	cout << "Writing actual flip angle to " << outPath << "..." << endl;
 	outFile.setnt(1);
 	outFile.setDatatype(NIFTI_TYPE_FLOAT32);
 	outFile.open(outPath, NiftiImage::NIFTI_WRITE);
@@ -108,12 +113,12 @@ int main(int argc, char **argv)
 	outFile.close();
 	
 	outPath = outPrefix + "_B1.nii.gz";
-	std::cout << "Writing B1 ratio to " << outPath << "..." << std::endl;
+	cout << "Writing B1 ratio to " << outPath << "..." << endl;
 	outFile.open(outPath, NiftiImage::NIFTI_WRITE);
 	outFile.writeVolume(0, B1);
 	outFile.close();
 	
-	std::cout << "Finished." << std::endl;
+	cout << "Finished." << endl;
     return EXIT_SUCCESS;
 }
 
--- phasemap_main.cpp
+++ phasemap_main.cpp
@@ -11,7 +11,14 @@
 #include "NiftiImage.h"
 #include "procpar.h"
 
-const std::string usage
+using namespace std;
+#define USE_PROCPAR
+#ifdef USE_PROCPAR
+	#include "procpar.h"
+	using namespace Recon;
+#endif
+
+const string usage
 {
 "Usage is: phasemap input_1 input_2 outprefix\n\
 \n\
@@ -37,15 +44,13 @@ int main(int argc, char** argv)
 	};
 	
 	int indexptr = 0, c;
-	std::string procPath;
-	par_t *pars;
 	double TE1, TE2, deltaTE, phasetime = 0.;
 	double *data1, *data2, *B0, *mask = NULL;
 	NiftiImage inFile, outFile;
 	while ((c = getopt_long(argc, argv, "m:", long_options, &indexptr)) != -1) {
 		switch (c) {
 			case 'm':
-				std::cout << "Reading mask from " << optarg << std::endl;
+				cout << "Reading mask from " << optarg << endl;
 				inFile.open(optarg, NiftiImage::NIFTI_READ);
 				mask = inFile.readVolume<double>(0);
 				inFile.close();
@@ -56,47 +61,56 @@ int main(int argc, char** argv)
 		}
 	}
 	if ((argc - optind) == 2) {
-		std::cout << "Opening input file " << argv[optind] << "." << std::endl;
+		cout << "Opening input file " << argv[optind] << "." << endl;
 		inFile.open(argv[optind], NiftiImage::NIFTI_READ);
-		procPath = inFile.basename() + ".procpar";
-		if ((pars = readProcpar(procPath.c_str()))) {
-			TE1 = realVal(pars, "te", 0);
-			TE2 = realVal(pars, "te", 1);
-		} else {
-			std::cout << "Enter TE2 & TE2 (seconds): ";
-			std::cin >> TE1 >> TE2;
+		
+		#ifdef USE_PROCPAR
+		ParameterList pars;
+		if (ReadProcpar(inFile.basename() + ".procpar", pars)) {
+			TE1 = RealValue(pars, "te", 0);
+			TE2 = RealValue(pars, "te", 1);
+		} else
+		#endif
+		{
+			cout << "Enter TE2 & TE2 (seconds): ";
+			cin >> TE1 >> TE2;
 		}
 		data1 = inFile.readVolume<double>(0);
 		data2 = inFile.readVolume<double>(1);
 		inFile.close();
 	} else if ((argc - optind) == 3) {
-		std::cout << "Opening input file 1" << argv[optind] << "." << std::endl;
+		cout << "Opening input file 1" << argv[optind] << "." << endl;
 		inFile.open(argv[optind], NiftiImage::NIFTI_READ);
-		procPath = inFile.basename() + ".procpar";
-		if ((pars = readProcpar(procPath.c_str())))
-			TE1 = realVal(pars, "te", 0);
-		else {
-			std::cout << "Enter TE1 (seconds): ";
-			std::cin >> TE1;
+		#ifdef USE_PROCPAR
+		ParameterList pars;
+		if (ReadProcpar(inFile.basename() + ".procpar", pars)) {
+			TE1 = RealValue(pars, "te", 0);
+		} else
+		#endif
+		{
+			cout << "Enter TE1 (seconds): ";
+			cin >> TE1;
 		}
 		data1 = inFile.readVolume<double>(0);
 		inFile.close();
-		std::cout << "Opening input file 2" << argv[++optind] << "." << std::endl;
+		cout << "Opening input file 2" << argv[++optind] << "." << endl;
 		inFile.open(argv[optind], NiftiImage::NIFTI_READ);
-		procPath = inFile.basename() + ".procpar";
-		if ((pars = readProcpar(procPath.c_str())))
-			TE2 = realVal(pars, "te", 0);
-		else {
-			std::cout << "Enter TE2 (seconds): ";
-			std::cin >> TE2;
+		#ifdef USE_PROCPAR
+		if (ReadProcpar(inFile.basename() + ".procpar", pars)){
+			TE2 = RealValue(pars, "te", 0);
+		} else
+		#endif
+		{
+			cout << "Enter TE2 (seconds): ";
+			cin >> TE2;
 		}
 		data2 = inFile.readVolume<double>(1);
 		inFile.close();
 	} else {
-		std::cerr << usage << std::endl;
+		cerr << usage << endl;
 		exit(EXIT_FAILURE);
 	}
-	std::string outPrefix(argv[++optind]);
+	string outPrefix(argv[++optind]);
 	if (TE2 < TE1) {	// Swap them
 		fprintf(stdout, "TE2 < TE1, swapping.\n");
 		double *tmp = data1;
@@ -107,9 +121,9 @@ int main(int argc, char** argv)
 		TE1 = tmpTE;
 	}
 	deltaTE = TE2 - TE1;
-	std::cout << "Delta TE = " << deltaTE << std::endl;
+	cout << "Delta TE = " << deltaTE << endl;
 	B0 = (double *)malloc(inFile.voxelsPerVolume() * sizeof(double));
-	std::cout << "Processing..." << std::endl;
+	cout << "Processing..." << endl;
 	for (size_t vox = 0; vox < inFile.voxelsPerVolume(); vox++) {
 		if (!mask || mask[vox] > 0.) {
 			double deltaPhase = data2[vox] - data1[vox];
@@ -122,15 +136,15 @@ int main(int argc, char** argv)
 			}
 		}
 	}
-	std::cout << "Writing B0 map." << std::endl;
-	std::string outPath = outPrefix + "_B0.nii.gz";
+	cout << "Writing B0 map." << endl;
+	string outPath = outPrefix + "_B0.nii.gz";
 	outFile = inFile;
 	outFile.setnt(1);
 	outFile.setDatatype(NIFTI_TYPE_FLOAT32);
 	outFile.open(outPath, NiftiImage::NIFTI_WRITE);
 	outFile.writeVolume(0, B0);
 	outFile.close();
-	std::cout << "Finished." << std::endl;
+	cout << "Finished." << endl;
     return EXIT_SUCCESS;
 }
 
