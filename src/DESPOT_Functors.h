cb0d2ab69dcaea301afc8cdd0028aae0fda4f7b4
Tobias Wood <tobias.wood@kcl.ac.uk>
2013-08-23 15:56:31 +0100

    Updated default 3T fitting bounds.

--- src/DESPOT_Functors.h
+++ src/DESPOT_Functors.h
@@ -227,14 +227,14 @@ class mcDESPOT : public DESPOTFunctor {
 			switch (m_fieldStrength) {
 				case FieldStrength::Three:
 					switch (m_components) {
-						case Components::One:   b.block(0, 0, 2, 2) << 0.25, 3.0, 0.01, 0.25; break;
-						case Components::Two:   b.block(0, 0, 6, 2) << 0.25, 1.0, 0.01, 0.05, 0.75, 1.5, 0.01, 0.05, 0.01, 0.5, 0.001, 0.95; break;
-						case Components::Three: b.block(0, 0, 9, 2) << 0.35, 0.55, 0.002, 0.016, 0.700, 2.0, 0.075, 0.145, 3.5, 7.5, 0.175, 0.5, 0.05, 0.3, 0.001, 0.3, 0.001, 0.95; break;
+						case Components::One:   b.block(0, 0, 2, 2) << 0.1, 4.0, 0.01, 1.5; break;
+						case Components::Two:   b.block(0, 0, 6, 2) << 0.1, 0.25, 0.002, 0.03, 0.7, 4.0, 0.075, 0.145, 0.05, 0.3, 0.0, 0.95; break;
+						case Components::Three: b.block(0, 0, 9, 2) << 0.1, 0.25, 0.002, 0.03, 0.7, 2.0, 0.075, 0.145, 3.5, 4.0, 0.8, 1.5, 0.05, 0.3, 0.001, 0.3, 0.0, 0.95; break;
 					} break;
 				case FieldStrength::Seven:
 					switch (m_components) {
-						case Components::One:   b.block(0, 0, 2, 2) << 0.25, 5.0, 0.01, 0.1; break;
-						case Components::Two:   b.block(0, 0, 6, 2) << 0.1, 0.5, 0.001, 0.025, 1.0, 2.5, 0.04, 0.08, 0.01, 0.25, 0.001, 1.0; break;
+						case Components::One:   b.block(0, 0, 2, 2) << 0.1, 4.0, 0.01, 2.0; break;
+						case Components::Two:   b.block(0, 0, 6, 2) << 0.1, 0.5, 0.001, 0.025, 1.0, 4.0, 0.04, 0.08, 0.01, 0.25, 0.001, 1.0; break;
 						case Components::Three: b.block(0, 0, 9, 2) << 0.1, 0.5, 0.001, 0.025, 1.0, 2.5, 0.04, 0.08, 3., 4.5, 0.5, 2.0, 0.01, 0.25, 0.001, 0.4, 0.001, 1.0; break;
 					} break;
 				case FieldStrength::Unknown:
