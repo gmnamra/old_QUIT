20483d4e0aac2c9821cfffeb5e518530bb89c036
Tobias Wood <tobias.wood@kcl.ac.uk>
2013-09-03 15:49:50 +0100

    Added options to control how multi-echo files are dealt with. Added a verbose option and an extra output per volume.

--- src/fdf2nii_main.cpp
+++ src/fdf2nii_main.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <iostream>
 #include <iterator>
+#include <algorithm>
 #include <getopt.h>
 
 #include "fdf.h"
@@ -15,7 +16,8 @@
 
 using namespace std;
 
-static bool zip = false, procpar = false;
+static bool zip = false, procpar = false, verbose = false;
+static int echoMode = -1;
 static double scale = 1.;
 static string outPrefix;
 static struct option long_options[] =
@@ -23,7 +25,9 @@ static struct option long_options[] =
 	{"scale", required_argument, 0, 's'},
 	{"out", required_argument, 0, 'o'},
 	{"zip", no_argument, 0, 'z'},
+	{"echo", required_argument, 0, 'e'},
 	{"procpar", no_argument, 0, 'p'},
+	{"verbose", no_argument, 0, 'v'},
 	{0, 0, 0, 0}
 };
 
@@ -37,18 +41,26 @@ Options:\n\
  -s, --scale:   Scale factor for image dimensions (set to 10 for use with SPM).\n\
  -o, --out:     Specify an output prefix.\n\
  -z, --zip:     Create .nii.gz files\n\
- -p, --procpar: Embed procpar in the nifti header.\n"
+ -e, --echo N:  Choose echo N in a multiple echo file. Valid values for N are:\n\
+                0..max echo   Write out just this echo\n\
+				-1 (default)  Write out all echoes as individual images.\n\
+				-2            Average echoes\n\
+				If an echo is chosen beyond the maximum nothing is written.\n\
+ -p, --procpar: Embed procpar in the nifti header.\n\
+ -v, --verbose: Print out extra info (e.g. after each volume is written).\n"
 };
 
 int main(int argc, char **argv) {
 	int indexptr = 0, c;
-	while ((c = getopt_long(argc, argv, "s:o:zp", long_options, &indexptr)) != -1) {
+	while ((c = getopt_long(argc, argv, "s:o:ze:pv", long_options, &indexptr)) != -1) {
 		switch (c) {
 			case 0: break; // It was an option that just sets a flag.
 			case 's': scale = atof(optarg); break;
 			case 'o': outPrefix = string(optarg); break;
 			case 'z': zip = true; break;
+			case 'e': echoMode = atoi(optarg); break;
 			case 'p': procpar = true; break;
+			case 'v': verbose = true; break;
 			default: cout << "Unknown option " << optarg << endl;
 		}
 	}
@@ -70,11 +82,18 @@ int main(int argc, char **argv) {
 
 		if (zip)
 			outPath += ".gz";
-		cout << "Converting " << inPath << " to " << outPath << endl;
+		if (verbose)
+			cout << "Converting " << inPath << " to " << outPath << "..." << endl;
 		try {
 			Agilent::fdfImage input(inPath);
+			size_t nOutImages = input.dim(3);
+			if (echoMode == -1) {
+				nOutImages *= input.dim(4);
+			} else if ((echoMode >= 0) && (echoMode >= input.dim(4))) {
+				throw(invalid_argument("Selected echo was above the maximum."));
+			}
 			try {
-				Nifti::File output(input.dim(0), input.dim(1), input.dim(2), input.dim(3),
+				Nifti::File output(input.dim(0), input.dim(1), input.dim(2), nOutImages,
 								   input.voxdim(0) * scale, input.voxdim(1) * scale, input.voxdim(2) * scale, 1.,
 								   DT_FLOAT32, input.ijk_to_xyz().cast<float>());
 				if (procpar) {
@@ -87,8 +106,24 @@ int main(int argc, char **argv) {
 					output.addExtension(NIFTI_ECODE_COMMENT, data);
 				}
 				output.open(outPath, Nifti::Modes::Write);
-				for (size_t v = 0; v < input.dim(3); v++) {
-					output.writeVolume<float>(v, input.readVolume<float>(v));
+				size_t outVol = 0;
+				for (size_t inVol = 0; inVol < input.dim(3); inVol++) {
+					if (echoMode >= 0) {
+						output.writeVolume<float>(outVol++, input.readVolume<float>(inVol, echoMode));
+					} else if (echoMode == -1) {
+						for (size_t e = 0; e < input.dim(4); e++) {
+							output.writeVolume<float>(outVol++, input.readVolume<float>(inVol, e));
+						}
+					} else if (echoMode == -2) {
+						vector<float> sum = input.readVolume<float>(inVol, 0);
+						for (size_t e = 1; e < input.dim(4); e++) {
+							vector<float> data = input.readVolume<float>(inVol, e);
+							transform(sum.begin(), sum.end(), data.begin(), sum.begin(), plus<float>());
+						}
+						output.writeVolume<float>(outVol++, sum);
+					}
+					if (verbose)
+						cout << "Wrote volume " << outVol << " of " << nOutImages << endl;
 				}
 				output.close();
 			} catch (exception &e) {
@@ -99,6 +134,8 @@ int main(int argc, char **argv) {
 			cerr << "Error, skipping to next input. " << e.what() << endl;
 			continue;
 		}
+		if (verbose)
+			cout << "Finished writing file " << outPath << endl;
 	}
     return EXIT_SUCCESS;
 }
