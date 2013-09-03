c3300b09d5b90c4570ee2af1b32fb5f6ba7a3230
Tobias Wood <tobias.wood@kcl.ac.uk>
2013-09-03 15:54:51 +0100

    Added a SimpleStatusBar from the effbot examples. Added multi-echo options.

--- python/fdf2nii.py
+++ python/fdf2nii.py
@@ -12,7 +12,21 @@ def mkdir_p(path):
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else: raise
+
+class StatusBar(Tk.Frame):
+    def __init__(self, master):
+		Tk.Frame.__init__(self, master)
+		self.label = Tk.Label(self, bd=1, relief=Tk.SUNKEN, anchor=Tk.W, width = 64)
+		self.label.pack(fill=Tk.X)
+		self.label.pack_propagate(False)
 	
+    def set(self, format, *args):
+		self.label.config(text=format % args)
+		self.label.update_idletasks()
+
+    def clear(self):
+		self.label.config(text="")
+		self.label.update_idletasks()
 
 class App:
 	def __init__(self, master):
@@ -36,27 +50,34 @@ class App:
 		options.grid(row = 1, column = 0)
 		self.study = Tk.IntVar()
 		self.study.set(1)
-		Tk.Checkbutton(options, text = "Whole study", variable = self.study).grid(row = 0, column = 0)
+		Tk.Checkbutton(options, text = "Whole study", variable = self.study).grid(row = 0, column = 0, sticky = Tk.W)
 		self.spm_scale = Tk.IntVar()
 		self.spm_scale.set(1)
-		Tk.Checkbutton(options, text = "Scale for SPM", variable = self.spm_scale).grid(row = 0, column = 1)
+		Tk.Checkbutton(options, text = "Scale for SPM", variable = self.spm_scale).grid(row = 0, column = 1, sticky = Tk.W)
 		self.embed_procpar = Tk.IntVar()
 		self.embed_procpar.set(1)
-		Tk.Checkbutton(options, text = "Embed procpar", variable = self.embed_procpar).grid(row = 0, column = 2)
+		Tk.Checkbutton(options, text = "Embed procpar", variable = self.embed_procpar).grid(row = 0, column = 2, sticky = Tk.W)
 		self.gz = Tk.IntVar()
 		self.gz.set(0)
-		Tk.Checkbutton(options, text = "Compress", variable = self.gz).grid(row = 0, column = 3)
+		Tk.Checkbutton(options, text = "Compress", variable = self.gz).grid(row = 0, column = 3, sticky = Tk.W)
 		self.ignore_scout = Tk.IntVar()
 		self.ignore_scout.set(1)
-		Tk.Checkbutton(options, text = "Ignore scouts", variable = self.ignore_scout).grid(row = 0, column = 4)
+		Tk.Checkbutton(options, text = "Ignore scouts", variable = self.ignore_scout).grid(row = 0, column = 4, sticky = Tk.W)
+		self.echo_mode = Tk.IntVar()
+		self.echo_mode.set(-1)
+		Tk.Label(options, text = "Multi-echo:").grid(row = 1, column = 0, sticky = Tk.E)
+		Tk.Radiobutton(options, text = "All", variable = self.echo_mode, value = -1).grid(row = 1, column = 1, sticky = Tk.W)
+		Tk.Radiobutton(options, text = "Sum", variable = self.echo_mode, value = -2).grid(row = 1, column = 2, sticky = Tk.W)
+		Tk.Radiobutton(options, text = "Select:", variable = self.echo_mode, value=0).grid(row = 1, column = 3, sticky = Tk.W)
+		self.echo_select = Tk.Spinbox(options, from_ = 0, to=999999, width = 8) # Hopefully that is high enough!
+		self.echo_select.grid(row = 1, column = 4, sticky = Tk.W)
 		
-		go = Tk.Frame(frame)
-		go.grid(row = 2)
-		self.go_button = Tk.Button(go, text = "Convert", command = self.go)
-		self.go_button.grid(row = 0, column = 2)
-		self.go_text = Tk.StringVar()
+		self.go_button = Tk.Button(options, text = "Convert", command = self.go)
+		self.go_button.grid(row = 0, column = 5, rowspan = 2, columnspan = 2, sticky=Tk.N+Tk.S+Tk.E+Tk.W)
+		self.go_text = StatusBar(frame)
 		self.go_text.set("Ready")
-		Tk.Label(go, textvariable = self.go_text, width=25).grid(row = 0, column = 0, columnspan = 2)
+		self.go_text.grid(row = 2, sticky=Tk.W + Tk.E)
+		self.go_text.grid_propagate(False)
 		
 		menu = Tk.Menu(root)
 		root.config(menu=menu)
@@ -79,7 +100,7 @@ class App:
 		(indir, inbase) = os.path.split(os.path.normpath(inpath))
 		outpath = self.out_entry.get()
 		
-		command = 'fdf2nii '
+		command = 'fdf2nii -v'
 		if self.spm_scale.get():
 			command = command + '-s 10.0 '
 		
@@ -88,6 +109,11 @@ class App:
 		
 		if self.gz.get():
 			command = command + '-z '
+		
+		if self.echo_mode.get() >= 0:
+			command = command + '-e ' + self.echo_select.get() + ' '
+		else:
+			command = command + '-e ' + str(self.echo_mode.get()) + ' '
 		
 		if self.study.get():
 			if inext == ".img":
@@ -112,16 +138,16 @@ class App:
 		#print command
 		self.go_text.set("Starting...")
 		self.master.config(cursor = "watch")
-		self.master.update()
+		self.master.update_idletasks()
 		p = subprocess.Popen(command,
 							 shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		for line in iter(p.stdout.readline, ''):
-			self.go_text.set(line)
-			print line,
-			self.master.update()
+			self.go_text.set(line.rstrip())
+			self.master.update_idletasks()
 		self.master.config(cursor = "")
 		self.go_text.set("Finished")
 
 root = Tk.Tk()
 app = App(root)
+root.resizable(0, 0)
 root.mainloop()
