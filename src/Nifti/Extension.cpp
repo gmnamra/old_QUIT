6a94a9e36f5a3b7da3abfa30b945d339696cda1f
Tobias Wood <tobias.wood@kcl.ac.uk>
2013-09-18 16:46:23 +0100

    Moved source files into new src/Nifti directory, so headers can be copied to include/Nifti/ easily.

---
+++ src/Nifti/Extension.cpp
@@ -0,0 +1,77 @@
+//
+//  Extension.cpp
+//  NiftiImage
+//
+//  Created by Tobias Wood on 18/09/2013.
+//  Copyright (c) 2013 Tobias Wood. All rights reserved.
+//
+
+#include "Nifti/Nifti.h"
+
+//*********************************
+#pragma mark Methods for Extension
+//*********************************
+const string &Nifti::Extension::CodeName(const int code) {
+	static const map<int, string> Codes {
+		{ NIFTI_ECODE_IGNORE,           "Ignore" },
+		{ NIFTI_ECODE_DICOM,            "DICOM Attributes" },
+		{ NIFTI_ECODE_AFNI,             "AFNI" },
+		{ NIFTI_ECODE_COMMENT,          "Plain ASCII text" },
+		{ NIFTI_ECODE_XCEDE,            "XCEDE" },
+		{ NIFTI_ECODE_JIMDIMINFO,       "JIM Dimension Information" },
+		{ NIFTI_ECODE_WORKFLOW_FWDS,    "Workflow Forwards" },
+		{ NIFTI_ECODE_FREESURFER,       "Freesurfer" },
+		{ NIFTI_ECODE_PYPICKLE,         "Pickled Python Objects" },
+		{ NIFTI_ECODE_MIND_IDENT,       "Mind Ident" },
+		{ NIFTI_ECODE_B_VALUE,          "B Value" },
+		{ NIFTI_ECODE_SPHERICAL_DIRECTION, "Spherical Direction" },
+		{ NIFTI_ECODE_DT_COMPONENT,     "DT Component" },
+		{ NIFTI_ECODE_SHC_DEGREEORDER,  "SHC Degree Order" },
+		{ NIFTI_ECODE_VOXBO,            "VOXBO" },
+		{ NIFTI_ECODE_CARET,            "CARET" }
+	};
+	static string unknown("Unknown extension code");
+	auto it = Codes.find(code);
+	if (it == Codes.end())
+		return unknown;
+	else
+		return it->second;
+}
+
+Nifti::Extension::Extension(int code, vector<char> data) :
+	m_code(code), m_data(data)
+{}
+Nifti::Extension::Extension(int size, int code, char *data) :
+	m_code(code)
+{
+	m_data.resize(size - 8);
+	for (int i = 0; i < (size - 8); i++) {
+		m_data[i] = data[i];
+	}
+}
+const int Nifti::Extension::rawSize() const {
+	return static_cast<int>(m_data.size());
+}
+const int Nifti::Extension::padding() const {
+	return static_cast<int>(16 - ((m_data.size() + 8) % 16));
+}
+const int Nifti::Extension::size() const {
+	// Must leave 8 bytes for the code and size fields (ints)
+	return rawSize() + 8 + padding();
+}
+
+const int Nifti::Extension::code() const { return m_code; }
+const string &Nifti::Extension::codeName() const { return CodeName(m_code); }
+void Nifti::Extension::setCode(int code) {
+	// Code must be in range and even
+	if ((code > NIFTI_ECODE_IGNORE) && (code < NIFTI_MAX_ECODE) && !(code & 1 ))
+		m_code = code;
+	else
+		throw(std::invalid_argument("Invalid Nifti::Extension code."));
+}
+
+const vector<char> &Nifti::Extension::data() const { return m_data; };
+void Nifti::Extension::setData(const vector<char> &data) {
+	m_data = data;
+}
+
---
+++ src/Nifti/nifti_hdr.cpp
@@ -0,0 +1,168 @@
+//
+//  nifti_hdr.cpp
+//  NiftiImage
+//
+//  Created by Tobias Wood on 08/07/2013.
+//  Copyright (c) 2013 Tobias Wood. All rights reserved.
+//
+
+#include <memory>
+#include <vector>
+#include <iostream>
+#include <getopt.h>
+
+#include "Nifti.h"
+
+using namespace std;
+
+const string usage = "nifti_hdr - A utility for getting information from Nifti headers.\n\
+\n\
+Usage: nifti_hdr [options] file1 [other files]\n\
+By default the abbreviated header will be printed for each file.\n\
+Abbreviated, full and compare modes are mutually exclusive.\n\
+\n\
+Options:\n\
+	-d, --dims :  Print the size of each dimension for each file.\n\
+	-v, --vox :   Print the voxel sizes for each file (with units). \n\
+	-t, --trans : Print the transform to physical space with precedence.\n\
+	-a, --abbrev: Print the abbreviated header.\n\
+	-f, --full:   Print the entire header.\n\
+	-c, --comp:   Compare first file to all others and print message if the.\n\
+	              physical spaces are different.\n\
+	-h, --help:   Print this message and quit.\n\
+";
+
+enum Modes {
+	Nothing = 0,
+	Abbreviated,
+	Full,
+	Compare
+};
+
+static int mode = Nothing;
+static int printDims = false, printVoxdims = false, printSize = false, printData = false,
+           printTransform = false, printExtensions = false;
+
+static struct option long_options[] =
+{
+	{"dims",   no_argument, &printDims, true},
+	{"vox",    no_argument, &printVoxdims, true},
+	{"form",   no_argument, &printTransform, true},
+	{"size",   no_argument, &printSize, true},
+	{"data",   no_argument, &printData, true},
+	{"abbrev", no_argument, &mode, Abbreviated},
+	{"full",   no_argument, &mode, Full},
+	{"comp",   no_argument, &mode, Compare},
+	{"help",   no_argument, 0, 'h'},
+	{"ext",    no_argument, 0, 'e'},
+	{0, 0, 0, 0}
+};
+
+string voxMessage(const Nifti &im) {
+	stringstream m;
+	m << "Voxel sizes: " << im.voxDims().transpose() << " " << im.spaceUnits();
+	if (im.voxDims().rows() > 3) {
+		m << "/" << im.timeUnits();
+	}
+	return m.str();
+}
+
+string sizeMessage(const Nifti &im) {
+	stringstream m;
+	m << "Voxels per slice, per volume, total: "
+      << im.voxelsPerSlice() << ", " << im.voxelsPerVolume() << ", " << im.voxelsTotal();
+	return m.str();
+}
+
+string dataMessage(const Nifti &im) {
+	stringstream m;
+	m << "Datatype: " << im.dtypeName() << ", size in bytes: " << im.bytesPerVoxel();
+	return m.str();
+}
+
+int main(int argc, char **argv) {
+	int indexptr = 0, c;
+	while ((c = getopt_long(argc, argv, "dvtafchse", long_options, &indexptr)) != -1) {
+		switch (c) {
+			case 'd': printDims = true; break;
+			case 'v': printVoxdims = true; break;
+			case 't': printTransform = true; break;
+			case 's': printSize = true; break;
+			case 'e': printExtensions = true; break;
+			case 'a': mode = Abbreviated; break;
+			case 'f': mode = Full; break;
+			case 'c': mode = Compare; break;
+			case '?': // getopt will print an error message
+			case 'h':
+				cout << usage << endl;
+				return EXIT_FAILURE;
+		}
+	}
+	if ((argc - optind) <= 0 ) {
+		cerr << "No input image file specified." << endl;
+		exit(EXIT_FAILURE);
+	}
+	if (optind == 1) { // No options specified, default is short header
+		mode = Abbreviated;
+	}
+	vector<Nifti> images;
+	images.reserve(argc - optind); // emplace_back can still trigger copies if the vector has to be resized
+	for (;optind < argc; optind++) {
+		images.emplace_back(argv[optind], Nifti::Mode::ReadHeader);
+	}
+
+	if (mode == Compare) { // Compare first image to all others and check headers are compatible
+		for (auto im = images.begin() + 1; im != images.end(); im++) {
+			if (!images[0].matchesSpace(*im)) {
+				cout << "Header does not match against file: " << im->imagePath() << endl;
+			}
+		}
+	}
+	
+	for (auto& im: images) {
+		if (printDims) cout << im.dims().transpose() << endl;
+		if (printVoxdims) cout << voxMessage(im) << endl;
+		if (printTransform) cout << im.transform().matrix() << endl;
+		if (printSize) cout << sizeMessage(im) << endl;
+		if (printData) cout << dataMessage(im) << endl;
+		
+		if (mode == Abbreviated) {
+			cout << "Short Nifti Header for file: " << im.imagePath() << endl;
+			cout << "Dimensions:  " << im.dims().transpose() << endl;
+			cout << voxMessage(im) << endl;
+			cout << "Transform matrix: " << endl << im.transform().matrix() << endl;
+			cout << "Number of extensions: " << im.extensions().size() << endl;
+		} else if (mode == Full) {
+			cout << "Full Nifti Header for file: " << im.imagePath() << endl;
+			cout << dataMessage(im) << endl;
+			cout << "Dimensions: " << im.dims().transpose() << endl;
+			cout << voxMessage(im) << endl;
+			
+			cout << "Calibration (min, max): " << im.calibration_min << ", " << im.calibration_max << endl;
+			cout << "Scaling (slope, inter): " << im.scaling_slope << ", " << im.scaling_inter << endl;
+			cout << "Dimension labels (Phase, Freq, Slice):   " << im.phase_dim << ", " << im.freq_dim << ", " << im.slice_dim << endl;
+			cout << "Slice info (Code, Start, End, Duration): " << ", " << im.slice_code << ", " << im.slice_start << ", " << im.slice_end << ", " << im.slice_duration << endl;
+			cout << "Slice name: " << im.sliceName() << endl;
+			cout << "Time offset: " << im.toffset << endl;
+			
+			cout << "Intent name:   " << im.intent_name << endl;
+			cout << "Intent code:   " << im.intentName() << endl;
+			cout << "Intent params: " << im.intent_p1 << ", " << im.intent_p2 << ", " << im.intent_p3 << endl;
+			cout << "Description: " << im.description << endl;
+			cout << "Aux File:    " << im.aux_file << endl;
+			cout << "QForm: " << im.qformName() << endl;
+			cout << im.qform().matrix() << endl;
+			cout << "SForm: " << im.sformName() << endl;
+			cout << im.sform().matrix() << endl;
+			cout << "Extensions: " << endl;
+			for (auto &e : im.extensions()) {
+				cout << "Extension Code: " << e.code() << endl;
+				string out(e.data().begin(), e.data().end());
+				cout << out << endl;
+			}
+		}
+	}
+	
+	return EXIT_SUCCESS;
+}
+
