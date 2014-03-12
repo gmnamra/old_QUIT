7ec885ee8ee3e7cea0db9662a52e3349dc3f79d4
Tobias Wood <tobias.wood@kcl.ac.uk>
2014-03-12 13:30:25 +0000

    Fixed outdated indices in validParameters()

--- src/Model.cpp
+++ src/Model.cpp
@@ -215,26 +215,26 @@ const ArrayXXd Model::bounds(const FieldStrength f) const {
 
 const bool Model::validParameters(const VectorXd &params) const {
 	// Negative T1/T2 makes no sense
-	if ((params[0] <= 0.) || (params[1] <= 0.))
+	if ((params[1] <= 0.) || (params[2] <= 0.))
 		return false;
 	
 	switch (m_nC) {
 		case Signal::Components::One : return true;
 		case Signal::Components::Two :
 			// Check that T1_a, T2_a < T1_b, T2_b and that f_a makes sense
-			if ((params[0] < params[2]) &&
-				(params[1] < params[3]) &&
-				(params[5] <= 1.0))
+			if ((params[1] < params[3]) &&
+				(params[2] < params[4]) &&
+				(params[6] <= 1.0))
 				return true;
 			else
 				return false;
 		case Signal::Components::Three :
 			// Check that T1/2_a < T1/2_b < T1/2_c and that f_a + f_c makes sense
-			if ((params[0] < params[2]) &&
-				(params[1] < params[3]) &&
+			if ((params[1] < params[3]) &&
 				(params[2] < params[4]) &&
 				(params[3] < params[5]) &&
-				((params[7] + params[8]) <= 1.0))
+				(params[4] < params[6]) &&
+				((params[8] + params[9]) <= 1.0))
 				return true;
 			else
 				return false;
