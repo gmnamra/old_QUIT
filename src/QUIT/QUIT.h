8b1fd4a552be92649eac23069a34b8520c9892c6
Tobias Wood <tobias.wood@kcl.ac.uk>
2014-04-29 15:34:40 +0100

    Moved the credit/version information into a new QUIT header which also includes the rest of the libQUIT headers.

---
+++ src/QUIT/QUIT.h
@@ -0,0 +1,29 @@
+#ifndef QUIT_H
+#define QUIT_H
+
+#include <string>
+
+//******************************************************************************
+#pragma mark Versioning etc.
+//******************************************************************************
+#include "version"
+
+const std::string credit_shared {
+"Written by tobias.wood@kcl.ac.uk, based on work by Sean Deoni. \n\
+Acknowledgements greatfully received, grant discussions welcome."
+};
+
+const std::string credit_me {
+"Written by tobias.wood@kcl.ac.uk. \n\
+Acknowledgements greatfully received, grant discussions welcome."
+};
+
+//******************************************************************************
+#pragma Rest of the library
+//******************************************************************************
+#include "QUIT/MultiArray.h"
+#include "QUIT/Volume.h"
+#include "QUIT/VolumeFunctions.h"
+#include "QUIT/ThreadPool.h"
+
+#endif // QUIT_H
