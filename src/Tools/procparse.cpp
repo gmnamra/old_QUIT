40aa5ef123f3b6ecb5ec1d264c89a345322ea6a9
Tobias Wood <tobias.wood@kcl.ac.uk>
2014-06-20 16:38:53 +0100

    Updated the output of procparse to make it play better with command-line tools.

--- src/Tools/procparse.cpp
+++ src/Tools/procparse.cpp
@@ -19,12 +19,12 @@
 using namespace std;
 using namespace Agilent;
 
-static int full = false, partial = false, verbose = false;
+static int partial = false, verbose = false, verbatim = false;
 static struct option long_options[] =
 {
-	{"full", no_argument, &full, true},
 	{"partial", no_argument, &partial, true},
 	{"verbose", no_argument, &verbose, true},
+	{"verbatim", no_argument, &verbatim, true},
 	{"in", required_argument, 0, 'i'},
 	{0, 0, 0, 0}
 };
@@ -35,23 +35,23 @@ Usage: procparse [opts] file1 par1 par2 ... parN\n\
 par1 to parN are parameter names to search for in procpar. If none are specified \
 then the whole file will be listed.\n\
 Options:\n\
- -f, --full:       Print the full parameter information, not a shortened version.\n\
- -p, --partial:    Print parameters that are partial matches.\n\
- -i, --in file:    Check additional procpar files (can specify more than once).\n\
- -v, --verbose:    Print more information.\n"
+ --verbose, -v  : Print filenames, parameter names, and other information.\n\
+ --verbatim, -b : Print the full parameter definition\n\
+ --partial, -p  : Allow partial matches for parameter names\n\
+ --in file, -i  : Read additional procpar files (can specify more than once).\n"
 };
 
 int main(int argc, char **argv) {
 	int indexptr = 0, c;
 	list<string> paths;
-	while ((c = getopt_long(argc, argv, "fphvi:", long_options, &indexptr)) != -1) {
+	while ((c = getopt_long(argc, argv, "phvbi:", long_options, &indexptr)) != -1) {
 		switch (c) {
 			case 0: break; // It was an option that just sets a flag.
-			case 'f': full = true; break;
-			case 'p': partial = true; break;
-			case 'h': cout << usage << endl; break;
 			case 'v': verbose = true; break;
+			case 'b': verbatim = true; break;
+			case 'p': partial = true; break;
 			case 'i': paths.emplace_back(optarg); break;
+			case 'h': cout << usage << endl; break;
 			default: cout << "Unknown option " << optarg << endl;
 		}
 	}
@@ -86,46 +86,51 @@ int main(int argc, char **argv) {
 				}
 			}
 		} catch (exception &e) {
-			cout << "Could not read file " << p << ", not a valid procpar file." << endl;
+			cerr << e.what() << endl;
+			exit(EXIT_FAILURE);
 		}
 	}
 	
 	while (optind < argc) {
 		string searchName(argv[optind]);
-		if (verbose) {
-			cout << "Searching for parameter: " << searchName << endl;
-		}
 		auto pp_it = pps.begin();
 		auto path_it = paths.begin();
 		for (; pp_it != pps.end() && path_it != paths.end(); pp_it++, path_it++) {
 			if (verbose) {
-				cout << "In file: " << *path_it << endl;
+				cout << *path_it << " ";
 			}
-			//for (auto &n : pp_it->names())
-			//	cout << n << endl;
-			
 			if (partial) {
 				size_t matches(0);
 				vector<string> names = pp_it->names();
+				if (verbose)
+					cout << "Partial matches for: " << searchName << endl;
 				for (auto &n : names) {
 					if (n.find(searchName) != string::npos) {
-						if (full) {
+						if (verbatim) {
 							cout << pp_it->parameter(n) << endl;
 						} else {
-							cout << n << ": " << pp_it->parameter(n).print_values() << endl;
+							if (verbose) {
+								cout << n << ": ";
+							}
+							cout << pp_it->parameter(n).print_values() << endl;
 						}
 						matches++;
 					}
 				}
-				cout << matches << " matches." << endl;
+				if (verbose)
+					cout << matches << " matches." << endl << endl;
 			} else if (pp_it->contains(searchName)) {
-				if (full)
+				if (verbatim) {
 					cout << pp_it->parameter(searchName) << endl;
-				else
-					cout << searchName << ": " << pp_it->parameter(searchName).print_values() << endl;
+				} else {
+					if (verbose) {
+						cout << searchName << ": ";
+					}
+					cout << pp_it->parameter(searchName).print_values() << endl;
+				}
 			} else {
 				if (verbose)
-					cout << "Not found." << endl;
+					cout << "Parameter not found: " << searchName << endl;
 			}
 		}
 		optind++;
