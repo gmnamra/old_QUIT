193323dee8a4f2d65cf4a1d2bd4938d337570a90
Tobias Wood <tobias.wood@kcl.ac.uk>
2014-07-01 14:04:29 +0100

    Small tweaks to documentation.

--- doc/latex/run_pandoc.sh
+++ doc/latex/run_pandoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc -s --toc --mathml --highlight-style monochrome DESPOT_manual.tex -o HTML_Test/man.html --to=html5 --bibliography=/Users/Tobias/Documents/Kings/Bibliography/rat_bib.bib --css=/Users/Tobias/Code/QUIT/man/DESPOT/test.css
+pandoc -s --toc --mathml --highlight-style monochrome DESPOT.tex -o ../html/despot.html --to=html5 --bibliography=/Users/Tobias/Documents/Kings/Bibliography/rat_bib.bib --css=/Users/Tobias/Code/QUIT/man/DESPOT/test.css
