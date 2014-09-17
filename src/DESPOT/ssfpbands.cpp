fe7220665c415970bcad69225937f96804981cf2
Tobias Wood <tobias.wood@kcl.ac.uk>
2014-09-17 16:50:55 +0100

    Massively cleaned up code, implemented a version of the algorithm that should cope with more than 4 phase-cycles.

--- src/DESPOT/ssfpbands.cpp
+++ src/DESPOT/ssfpbands.cpp
@@ -33,42 +33,52 @@ Input must be a single complex image with 0, 90, 180, 360 phase-cycles in order\
 along the 4th dimension.\n\
 \n\
 Options:\n\
-	--help, -h        : Print this message\n\
-	--verbose, -v     : Print more information\n\
-	--out, -o path    : Specify an output filename (default image base)\n\
-	--mask, -m file   : Mask input with specified file\n\
-	--flip, -f        : Data order is flip-angle, then phase (default opposite)\n\
-	--save, -s r      : Save the line-regularised image (default)\n\
-	           m      : Save the magnitude-regularised image\n\
-	           e      : Save the ellipse cross-point\n\
-	           c      : Save the complex sum/average\n"
+	--help, -h        : Print this message.\n\
+	--verbose, -v     : Print more information.\n\
+	--out, -o path    : Specify an output filename (default image base).\n\
+	--mask, -m file   : Mask input with specified file.\n\
+	--flip, -f        : Data order is flip-angle, then phase (default opposite).\n\
+	--phases, -p N    : Number of phase-cycling patterns used (default is 4).\n\
+	--threads, -T N   : Use N threads (default=hardware limit).\n"
 };
 
-enum class SaveMode { LineReg, MagReg, CrossPoint, ComplexSum, Lambda, Mu };
 static bool verbose = false;
-static size_t phase_dim = 3, flip_dim = 4;
+static size_t phase_dim = 3, flip_dim = 4, nPhases = 4;
 static string prefix;
-static SaveMode save = SaveMode::LineReg;
-static struct option long_options[] =
-{
+static struct option long_options[] = {
 	{"help", no_argument, 0, 'h'},
 	{"verbose", no_argument, 0, 'v'},
 	{"out", required_argument, 0, 'o'},
 	{"mask", required_argument, 0, 'm'},
 	{"flip", required_argument, 0, 'f'},
-	{"save", required_argument, 0, 's'},
+	{"phases", required_argument, 0, 'p'},
+	{"threads", required_argument, 0, 'T'},
 	{0, 0, 0, 0}
 };
+
+// From Knuth, surprised this isn't in STL
+unsigned long long choose(unsigned long long n, unsigned long long k) {
+	if (k > n)
+		return 0;
+
+	unsigned long long r = 1;
+	for (unsigned long long d = 1; d <= k; ++d) {
+		r *= n--;
+		r /= d;
+	}
+	return r;
+}
+
 //******************************************************************************
 // Main
 //******************************************************************************
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	Nifti::File maskFile;
 	MultiArray<int8_t, 3> maskData;
-	
+	ThreadPool threads;
+
 	int indexptr = 0, c;
-	while ((c = getopt_long(argc, argv, "hvo:m:fs:", long_options, &indexptr)) != -1) {
+	while ((c = getopt_long(argc, argv, "hvo:m:fs:p:T:", long_options, &indexptr)) != -1) {
 		switch (c) {
 			case 'v': verbose = true; break;
 			case 'm':
@@ -83,18 +93,19 @@ int main(int argc, char **argv)
 				break;
 			case 'f':
 				phase_dim = 4; flip_dim = 3; break;
-			case 's':
-				switch (*optarg) {
-					case 'r': save = SaveMode::LineReg; break;
-					case 'm': save = SaveMode::MagReg; break;
-					case 'e': save = SaveMode::CrossPoint; break;
-					case 'c': save = SaveMode::ComplexSum; break;
-					case 'l': save = SaveMode::Lambda; break;
-					case 'u': save = SaveMode::Mu; break;
-					default:
-						cout << "Unknown regularisation mode '" << *optarg << "'" << endl;
-						return EXIT_FAILURE;
+			case 'p':
+				nPhases = atoi(optarg);
+				if ((nPhases % 2) != 0) {
+					cerr << "Number of phase-cycling patterns must be divisible by 2." << endl;
+					return EXIT_FAILURE;
 				}
+				if (nPhases < 4) {
+					cerr << "Must have a minimum of 4 phase-cycling patterns." << endl;
+					return EXIT_FAILURE;
+				}
+				break;
+			case 'T':
+				threads.resize(atoi(optarg));
 				break;
 			case 'h':
 			case '?': // getopt will print an error message
@@ -114,129 +125,94 @@ int main(int argc, char **argv)
 		cerr << "Mask does not match input file." << endl;
 		return EXIT_FAILURE;
 	}
-	if ((inputFile.rank() < 4) || ((inputFile.dim(4) % 4) != 0)) {
-		cout << "Input must contain 4 phase-cycles (0, 90, 180, 270)." << endl;
+	const auto d = inputFile.matrix();
+	size_t nFlip = inputFile.dim(4) / nPhases;
+	if (nFlip < 1) {
+		cerr << "The specified number of phase-cycling patterns is inconsistent with the number of volumes in the input file." << endl;
 		return EXIT_FAILURE;
 	}
+	size_t nLines = nPhases / 2;
+	size_t nCrossings = choose(nLines, 2);
+
+	if (verbose) cout << "Reading data..." << endl;
 	MultiArray<complex<float>, 4> input(inputFile.dims().head(4));
 	inputFile.readVolumes(input.begin(), input.end());
 	if (prefix == "") {
-		prefix = inputFile.basePath();
+		prefix = inputFile.basePath() + "_";
 	}
 	inputFile.close();
 
-	// Results storage
-	const auto d = inputFile.matrix();
-	size_t nFlip = inputFile.dim(4) / 4;
-	MultiArray<complex<float>, 5>::Index nd; nd << d, 0, 0;
-	nd[phase_dim] = 4;
-	nd[flip_dim] = nFlip;
-	MultiArray<complex<float>, 5> CData = input.reshape<5>(nd);
-	MultiArray<float, 4> lm_out(d, nFlip), mu_out(d, nFlip), mabs_out(d, nFlip), leeway_out(d, nFlip);
-	MultiArray<complex<float>, 4> cs_out(d, nFlip), gs_out(d, nFlip), reg_out(d, nFlip);
-	MultiArray<int, 4> lreg_out(d, nFlip);
+	if (verbose) cout << "Prepping data..." << endl;
+	typedef typename MultiArray<complex<float>, 5>::Index idx_t;
+	idx_t reshape_dims; reshape_dims << d, 0, 0;
+	reshape_dims[phase_dim] = 4;
+	reshape_dims[flip_dim] = nFlip;
+	MultiArray<complex<float>, 5> reshaped = input.reshape<5>(reshape_dims);
+	idx_t split_start = idx_t::Zero();
+	reshape_dims[phase_dim] = 2;
+	MultiArray<complex<float>, 5> aData = reshaped.slice<5>(split_start, reshape_dims);
+	split_start[phase_dim] = 2;
+	MultiArray<complex<float>, 5> bData = reshaped.slice<5>(split_start, reshape_dims);
+	// For results
+	MultiArray<complex<float>, 4> reg_out(d, nFlip);
 	//**************************************************************************
 	// Do the fitting
 	//**************************************************************************
 	clock_t startClock = clock();
-	ThreadPool pool;
 	for (size_t vol = 0; vol < nFlip; vol++) {
 		if (verbose) cout << "Processing volume " << vol << "..." << endl;
-		function<void (const size_t)> processVox = [&] (const size_t k) {
-			for (size_t j = 0; j < d[1]; j++) {
-				for (size_t i = 0; i < d[0]; i++) {
-					MultiArray<complex<float>, 5>::Index idx;
-					idx << i,j,k,0,0;
-					idx[flip_dim] = vol;
+		for (size_t vk = 0; vk < d[2]; vk++) {
+			function<void (const size_t, const size_t)> processVox = [&] (const size_t vi, const size_t vj) {
+				MultiArray<complex<float>, 5>::Index idx;
+				idx << vi,vj,vk,0,0;
+				idx[flip_dim] = vol;
 
-					complex<float> c_0   = CData[idx]; idx[phase_dim] = 1;
-					complex<float> c_90  = CData[idx]; idx[phase_dim] = 2;
-					complex<float> c_180 = CData[idx]; idx[phase_dim] = 3;
-					complex<float> c_270 = CData[idx];
+				MatrixXf solutions(2, nCrossings); solutions.setZero();
+				size_t si = 0;
+				for (size_t li = 0; li < nLines; li++) {
+					idx_t idx_i; idx_i << vi,vj,vk,0,0;
+					idx_i[flip_dim] = vol;
+					idx_i[phase_dim] = li;
+					for (size_t lj = li + 1; lj < nLines; lj++) {
+						idx_t idx_j = idx_i;
+						idx_j[phase_dim] = lj;
 
-					Vector2f v_0{c_0.real(), c_0.imag()};
-					Vector2f v_90{c_90.real(), c_90.imag()};
-					Vector2f v_180{c_180.real(), c_180.imag()};
-					Vector2f v_270{c_270.real(), c_270.imag()};
+						Vector2f a_i{aData[idx_i].real(), aData[idx_i].imag()};
+						Vector2f a_j{aData[idx_j].real(), aData[idx_j].imag()};
+						Vector2f b_i{bData[idx_i].real(), bData[idx_i].imag()};
+						Vector2f b_j{bData[idx_j].real(), bData[idx_j].imag()};
 
-					Vector2f d_0 = v_180 - v_0;
-					Vector2f d_90 = v_270 - v_90;
-					Vector2f n_0(d_0[1], -d_0[0]);
-					Vector2f n_90(d_90[1], -d_90[0]);
+						Vector2f d_i = (b_i - a_i);
+						Vector2f d_j = (b_j - a_j);
+						Vector2f n_i{d_i[1], -d_i[0]};
+						Vector2f n_j{d_j[1], -d_j[0]};
 
-					complex<float> cs = (c_0 + c_90 + c_180 + c_270) / complex<float>(4.0,0.0);
-					cs_out[{i,j,k,vol}] = cs;
+						float mu = (a_j - a_i).dot(n_j) / d_i.dot(n_j);
+						float nu = (a_i - a_j).dot(n_i) / d_j.dot(n_i);
+						float xi = 1.0 - pow(d_i.dot(d_j) / (d_i.norm() * d_j.norm()),2.0);
 
-					float lm_0 = ((v_90 - v_0).dot(n_90)) / (d_0.dot(n_90));
-					float lm_90 = ((v_0 - v_90).dot(n_0)) / (d_90.dot(n_0));
-
-					Vector2f gs_0 = (v_0 + lm_0 * d_0);
-					Vector2f gs_90 = (v_90 + lm_90 * d_90);
-
-					gs_out[{i,j,k,vol}] = {gs_0[0], gs_0[1]};
-					lm_out[{i,j,k,vol}] = lm_0;
-					mu_out[{i,j,k,vol}] = lm_90;
-
-					float max_abs = max(max(max(abs(c_0),abs(c_90)),abs(c_180)),abs(c_270));
-					bool mreg = gs_0.norm() > max_abs;
-					bool lreg = false;
-
-					float leeway = 1. - fabs(d_0.dot(d_90) / (d_0.norm() * d_90.norm()));
-
-
-					if (lm_0 < -leeway) { lreg = true; lm_0 = 0; }
-					if (lm_0 > (1. + leeway)) { lreg = true; lm_0 = 1; }
-					if (lm_90 < -leeway) { lreg = true; lm_90 = 0; }
-					if (lm_90 > (1. + leeway)) { lreg = true; lm_90 = 1; }
-					gs_0 = (v_0 + lm_0 * d_0);
-					gs_90 = (v_90 + lm_90 * d_90);
-
-					mabs_out[{i,j,k,vol}] = max_abs;
-					leeway_out[{i,j,k,vol}] = leeway;
-					lreg_out[{i,j,k,vol}] = lreg;
-
-					reg_out[{i,j,k,vol}] = lreg ? cs : gs_out[{i,j,k,vol}];
+						Vector2f cs = (a_i + a_j + b_i + b_j) / 4.0;
+						Vector2f gs = a_i + mu * d_i;
+						bool regularise = true;
+						// Do the logic this way round so NaN does not propagate
+						if ((mu > -xi) && (mu < 1 + xi) && (nu > -xi) && (nu < 1 + xi))
+							regularise = false;
+						solutions.col(si++) = regularise ? cs : gs;
+					}
 				}
-			}
-		};
-		pool.for_loop(processVox, d[2]);
+				Vector2f mean_sol = solutions.rowwise().mean();
+				reg_out[{vi,vj,vk,vol}] = {mean_sol[0], mean_sol[1]};
+			};
+			threads.for_loop2(processVox, d[0], d[1]);
+		}
 	}
 	printElapsedClock(startClock, d.prod());
 	inHdr.setDim(4, nFlip);
 	inHdr.setDatatype(Nifti::DataType::COMPLEX64);
 
-	string outname = prefix + "_lreg" + OutExt();
+	string outname = prefix + "reg" + OutExt();
 	Nifti::File outFile(inHdr, outname);
-	outFile.writeVolumes(lreg_out.begin(), lreg_out.end());
-	outFile.close();
-	outname = prefix + "_lw" + OutExt();
-	outFile.open(outname, Nifti::Mode::Write);
-	outFile.writeVolumes(leeway_out.begin(), leeway_out.end());
-	outFile.close();
-	outname = prefix + "_mabs" + OutExt();
-	outFile.open(outname, Nifti::Mode::Write);
-	outFile.writeVolumes(mabs_out.begin(), mabs_out.end());
-	outFile.close();
-	outname = prefix + "_cs" + OutExt();
-	outFile.open(outname, Nifti::Mode::Write);
-	outFile.writeVolumes(cs_out.begin(), cs_out.end());
-	outFile.close();
-	outname = prefix + "_gs" + OutExt();
-	outFile.open(outname, Nifti::Mode::Write);
-	outFile.writeVolumes(gs_out.begin(), gs_out.end());
-	outFile.close();
-	outname = prefix + "_reg" + OutExt();
-	outFile.open(outname, Nifti::Mode::Write);
 	outFile.writeVolumes(reg_out.begin(), reg_out.end());
-	outFile.close();
-	inHdr.setDatatype(Nifti::DataType::FLOAT64);
-	outname = prefix + "_lm" + OutExt();
-	outFile.open(outname, Nifti::Mode::Write);
-	outFile.writeVolumes(lm_out.begin(), lm_out.end());
-	outFile.close();
-	outname = prefix + "_mu" + OutExt();
-	outFile.open(outname, Nifti::Mode::Write);
-	outFile.writeVolumes(mu_out.begin(), mu_out.end());
 	outFile.close();
 	if (verbose) cout << "Finished." << endl;
 	return EXIT_SUCCESS;
