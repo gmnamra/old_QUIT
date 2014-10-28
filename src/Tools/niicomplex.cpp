2c73c9ccc78a41b115ba265d3fc12bea9fc76198
Tobias Wood <tobias.wood@kcl.ac.uk>
2014-10-28 11:45:23 +0000

    Made the output image type selection more flexible.

--- src/Tools/niicomplex.cpp
+++ src/Tools/niicomplex.cpp
@@ -12,6 +12,7 @@
 
 #include <getopt.h>
 #include <iostream>
+#include <functional>
 
 #include "Nifti/Nifti.h"
 #include "QUIT/MultiArray.h"
@@ -26,17 +27,21 @@ const string usage {
 \n\
 Default mode is to convert a magnitude/phase image pair into a real/imaginary \n\
 image pair. If you have/want different inputs/outputs, then specify the -i/-o \n\
-options. The correct number of input/output names must be given as arguments.\n\
+options. Multiple output types can be chosen. The correct number of names must\n\
+be given as additional arguments.\n\
 \n\
 Options:\n\
-	--help, -h        : Print this message\n\
-	--verbose, -v     : Print more information\n\
-	--input, -i m     : Input is magnitude/phase (default)\n\
-	            r     : Input is real/imaginary\n\
-	            c     : Input is complex\n\
-	--output, -o m    : Output will be magnitude/phase images\n\
-	             r    : Output will be real/imaginary images (default)\n\
-	             c    : Output will be a single complex image\n\
+	--help, -h           : Print this message\n\
+	--verbose, -v        : Print more information\n\
+	--input, -i m        : Input is magnitude/phase (default)\n\
+	            r        : Input is real/imaginary\n\
+	            c        : Input is complex\n\
+	--output, -o [mpric] : Where any of [mpric] are present\n\
+	             m       : Output a magnitude image\n\
+	             p       : Output a phase image\n\
+	             r       : Output a real image\n\
+	             i       : Output an imaginary image\n\
+	             c       : Output a complex image\n\
 	--dtype, -d f     : Force output datatype to float\n\
 	            d     : Force output datatype to double\n\
 	            l     : Force output datatype to long double\n"
@@ -44,7 +49,8 @@ Options:\n\
 
 enum class Type { MagPhase, RealImag, Complex };
 static bool verbose = false, forceDType = false;
-static Type inputType = Type::MagPhase, outputType = Type::RealImag;
+static Type inputType = Type::MagPhase;
+static string outputImages{"ri"};
 static DataType precision = DataType::FLOAT32;
 static struct option long_options[] =
 {
@@ -85,31 +91,39 @@ void re_im_to_cmp(Nifti::File &in1, Nifti::File &in2, size_t vol,
 }
 
 template<typename T>
-void cmp_to_mag(Nifti::File &out1, Nifti::File &out2, size_t vol,
-                vector<T> &v1, vector<T> &v2,
-                vector<complex<T>> &c)
+void write_mag_vol(Nifti::File &out, size_t vol,
+                   vector<complex<T>> &c, vector<T> &s)
 {
-	for (size_t i = 0; i < v1.size(); i++) {
-		v1[i] = abs(c[i]); v2[i] = arg(c[i]);
-	}
-	if (verbose) cout << "Writing magnitude volume " << vol << endl;
-	out1.writeVolumes(v1.begin(), v1.end(), vol, 1);
-	if (verbose) cout << "Writing phase volume " << vol << endl;
-	out2.writeVolumes(v2.begin(), v2.end(), vol, 1);
+	if (verbose) cout << "Writing magnitude volume..." << endl;
+	for (size_t i = 0; i < s.size(); i++) { s[i] = abs(c[i]); }
+	out.writeVolumes(s.begin(), s.end(), vol, 1);
 }
 
 template<typename T>
-void cmp_to_re_im(Nifti::File &out1, Nifti::File &out2, size_t vol,
-                  vector<T> &v1, vector<T> &v2,
-                  vector<complex<T>> &c)
+void write_ph_vol(Nifti::File &out, size_t vol,
+                  vector<complex<T>> &c, vector<T> &s)
 {
-	for (size_t i = 0; i < v1.size(); i++) {
-		v1[i] = c[i].real(); v2[i] = c[i].imag();
-	}
-	if (verbose) cout << "Writing real volume " << vol << endl;
-	out1.writeVolumes(v1.begin(), v1.end(), vol, 1);
-	if (verbose) cout << "Writing imaginary volume " << vol << endl;
-	out2.writeVolumes(v2.begin(), v2.end(), vol, 1);
+	if (verbose) cout << "Writing phase volume..." << endl;
+	for (size_t i = 0; i < s.size(); i++) { s[i] = arg(c[i]); }
+	out.writeVolumes(s.begin(), s.end(), vol, 1);
+}
+
+template<typename T>
+void write_real_vol(Nifti::File &out, size_t vol,
+                    vector<complex<T>> &c, vector<T> &s)
+{
+	if (verbose) cout << "Writing real volume..." << endl;
+	for (size_t i = 0; i < s.size(); i++) { s[i] = real(c[i]); }
+	out.writeVolumes(s.begin(), s.end(), vol, 1);
+}
+
+template<typename T>
+void write_imag_vol(Nifti::File &out, size_t vol,
+                    vector<complex<T>> &c, vector<T> &s)
+{
+	if (verbose) cout << "Writing imaginary volume..." << endl;
+	for (size_t i = 0; i < s.size(); i++) { s[i] = imag(c[i]); }
+	out.writeVolumes(s.begin(), s.end(), vol, 1);
 }
 
 //******************************************************************************
@@ -123,38 +137,30 @@ int main(int argc, char **argv)
 	int indexptr = 0, c;
 	while ((c = getopt_long(argc, argv, "hvi:o:d:", long_options, &indexptr)) != -1) {
 		switch (c) {
-			case 'v': verbose = true; break;
-			case 'i':
-				switch (*optarg) {
-					case 'm': inputType = Type::MagPhase;  cout << "Input is magnitude and phase." << endl; break;
-					case 'r': inputType = Type::RealImag; cout << "Input is real and imaginary." << endl; break;
-					case 'c': inputType = Type::Complex; cout << "Input is complex." << endl; break;
-					default:
-						cerr << "Unknown input type " << optarg << endl;
-						return EXIT_FAILURE;
-				} break;
-			case 'o':
-				switch (*optarg) {
-					case 'm': outputType = Type::MagPhase;  cout << "Output will be magnitude and phase." << endl; break;
-					case 'r': outputType = Type::RealImag; cout << "Output will be real and imaginary." << endl; break;
-					case 'c': outputType = Type::Complex; cout << "Output will be complex." << endl; break;
-					default:
-						cerr << "Unknown output type " << optarg << endl;
-						return EXIT_FAILURE;
-				} break;
-			case 'd':
-				switch (*optarg) {
-					case 'f': precision = DataType::FLOAT32; break;
-					case 'd': precision = DataType::FLOAT64; break;
-					case 'l': precision = DataType::FLOAT128; break;
-					default:
-						cerr << "Unknown precision type " << optarg << endl;
-						return EXIT_FAILURE;
-				} break;
-			case 'h':
-			case '?': // getopt will print an error message
-				cout << usage << endl;
-				return EXIT_SUCCESS;
+		case 'v': verbose = true; break;
+		case 'i':
+			switch (*optarg) {
+				case 'm': inputType = Type::MagPhase;  cout << "Input is magnitude and phase." << endl; break;
+				case 'r': inputType = Type::RealImag; cout << "Input is real and imaginary." << endl; break;
+				case 'c': inputType = Type::Complex; cout << "Input is complex." << endl; break;
+				default:
+					cerr << "Unknown input type " << optarg << endl;
+					return EXIT_FAILURE;
+			} break;
+		case 'o': outputImages = string(optarg); break;
+		case 'd':
+			switch (*optarg) {
+				case 'f': precision = DataType::FLOAT32; break;
+				case 'd': precision = DataType::FLOAT64; break;
+				case 'l': precision = DataType::FLOAT128; break;
+				default:
+					cerr << "Unknown precision type " << optarg << endl;
+					return EXIT_FAILURE;
+			} break;
+		case 'h':
+		case '?': // getopt will print an error message
+			cout << usage << endl;
+			return EXIT_SUCCESS;
 		}
 	}
 
@@ -164,11 +170,7 @@ int main(int argc, char **argv)
 		case Type::RealImag: expected_number_of_arguments += 2; break;
 		case Type::Complex:  expected_number_of_arguments += 1; break;
 	}
-	switch (outputType) {
-		case Type::MagPhase: expected_number_of_arguments += 2; break;
-		case Type::RealImag: expected_number_of_arguments += 2; break;
-		case Type::Complex: expected_number_of_arguments += 1; break;
-	}
+	expected_number_of_arguments += outputImages.size();
 	if (expected_number_of_arguments != (argc - optind)) {
 		cout << "Expected " << expected_number_of_arguments<<  " filenames, but " << (argc - optind) << " were given." << endl << usage << endl;
 		return EXIT_FAILURE;
@@ -186,25 +188,29 @@ int main(int argc, char **argv)
 		}
 	}
 
+	vector<File> outFiles;
 	Header outHdr = in1.header();
-	if (outputType == Type::Complex) {
-		switch (precision) {
-			case DataType::FLOAT32: outHdr.setDatatype(DataType::COMPLEX64); break;
-			case DataType::FLOAT64: outHdr.setDatatype(DataType::COMPLEX128); break;
-			case DataType::FLOAT128: outHdr.setDatatype(DataType::COMPLEX256); break;
-			default: throw(std::logic_error("Invalid precision type."));
+	for (size_t oi = 0; oi < outputImages.size(); oi++) {
+		char type = outputImages.at(oi);
+		switch (type) {
+		case 'm': case 'p': case 'r': case 'i':
+			outHdr.setDatatype(precision);
+			break;
+		case 'c':
+			switch (precision) {
+				case DataType::FLOAT32: outHdr.setDatatype(DataType::COMPLEX64); break;
+				case DataType::FLOAT64: outHdr.setDatatype(DataType::COMPLEX128); break;
+				case DataType::FLOAT128: outHdr.setDatatype(DataType::COMPLEX256); break;
+				default: throw(std::logic_error("Invalid precision type."));
+			}
+			break;
+		default:
+			// Need the string(1, type) constructor to build a string from a char
+			throw(std::runtime_error("Invalid output image type: " + string(1, type)));
+			break;
 		}
-	} else {
-		outHdr.setDatatype(precision);
-	}
-	File out1, out2;
-	if (verbose) cout << "Opening output file: " << argv[optind] << endl;
-	out1.setHeader(outHdr);
-	out1.open(argv[optind++], Nifti::Mode::Write);
-	if (outputType != Type::Complex) {
 		if (verbose) cout << "Opening output file: " << argv[optind] << endl;
-		out2.setHeader(outHdr);
-		out2.open(argv[optind++], Nifti::Mode::Write);
+		outFiles.emplace_back(File(outHdr, argv[optind++]));
 	}
 
 	// Allocate different types to save memory
@@ -225,7 +231,7 @@ int main(int argc, char **argv)
 	}
 
 	for (size_t vol = 0; vol < in1.dim(4); vol++) {
-		if (verbose) cout << "Converting volume " << vol << "..." << endl;
+		if (verbose) cout << "Converting volume " << vol << endl;
 		switch (inputType) {
 			case Type::MagPhase: {
 				switch (precision) {
@@ -253,37 +259,40 @@ int main(int argc, char **argv)
 				}
 			} break;
 		}
-		switch (outputType) {
-			case Type::MagPhase: {
+
+		for (size_t oi = 0; oi < outFiles.size(); oi++) {
+			#define DECL_SWITCH( FUNC )\
+			switch (precision) {\
+			case DataType::FLOAT32: FUNC<float>(outFiles.at(oi), vol, cmp_flt, flt1); break;\
+			case DataType::FLOAT64: FUNC<double>(outFiles.at(oi), vol, cmp_dbl, dbl1); break;\
+			case DataType::FLOAT128: FUNC<long double>(outFiles.at(oi), vol, cmp_ldbl, ldbl1); break;\
+			default: break;\
+			}
+
+			switch (outputImages.at(oi)) {
+			case 'm': DECL_SWITCH( write_mag_vol ) break;
+			case 'p': DECL_SWITCH( write_ph_vol ) break;
+			case 'r': DECL_SWITCH( write_real_vol ) break;
+			case 'i': DECL_SWITCH( write_imag_vol ) break;
+			case 'c':
+				if (verbose) cout << "Writing complex volume..." << endl;
 				switch (precision) {
-					case DataType::FLOAT32:  cmp_to_mag<float>(out1, out2, vol, flt1, flt2, cmp_flt); break;
-					case DataType::FLOAT64:  cmp_to_mag<double>(out1, out2, vol, dbl1, dbl2, cmp_dbl); break;
-					case DataType::FLOAT128: cmp_to_mag<long double>(out1, out2, vol, ldbl1, ldbl2, cmp_ldbl); break;
+					case DataType::FLOAT32: outFiles.at(oi).writeVolumes(cmp_flt.begin(), cmp_flt.end(), vol, 1); break;
+					case DataType::FLOAT64: outFiles.at(oi).writeVolumes(cmp_dbl.begin(), cmp_dbl.end(), vol, 1); break;
+					case DataType::FLOAT128: outFiles.at(oi).writeVolumes(cmp_ldbl.begin(), cmp_ldbl.end(), vol, 1); break;
 					default: break;
 				}
-			} break;
-			case Type::RealImag: {
-				switch (precision) {
-					case DataType::FLOAT32:  cmp_to_re_im<float>(out1, out2, vol, flt1, flt2, cmp_flt); break;
-					case DataType::FLOAT64:  cmp_to_re_im<double>(out1, out2, vol, dbl1, dbl2, cmp_dbl); break;
-					case DataType::FLOAT128: cmp_to_re_im<long double>(out1, out2, vol, ldbl1, ldbl2, cmp_ldbl); break;
-					default: break;
-				}
-			} break;
-			case Type::Complex : {
-				if (verbose) cout << "Writing complex volume " << vol << endl;
-				switch (precision) {
-					case DataType::FLOAT32: out1.writeVolumes(cmp_flt.begin(), cmp_flt.end(), vol, 1); break;
-					case DataType::FLOAT64: out1.writeVolumes(cmp_dbl.begin(), cmp_dbl.end(), vol, 1); break;
-					case DataType::FLOAT128: out1.writeVolumes(cmp_ldbl.begin(), cmp_ldbl.end(), vol, 1); break;
-					default: break;
-				}
-			} break;
+				break;
+			default:
+				break;
+			}
+
+			#undef DECL_SWITCH
 		}
 	}
-	out1.close();
-	if (outputType != Type::Complex) {
-		out2.close();
+
+	for (File& of: outFiles) {
+		of.close();
 	}
 
 	return EXIT_SUCCESS;
