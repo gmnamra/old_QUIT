da676df988a808bf1ead084336246e2cc7209a67
Tobias Wood <tobias.wood@kcl.ac.uk>
2014-10-28 16:18:22 +0000

    Added options to niihdr to print out voxdims, TR, transform. Removed compare mode.

--- src/Tools/niihdr.cpp
+++ src/Tools/niihdr.cpp
@@ -19,25 +19,27 @@ const string usage = "niihdr - A utility for getting information from Nifti head
 \n\
 Usage: niihdr [options] file1 [other files]\n\
 By default the abbreviated header will be printed for each file.\n\
-Abbreviated, full and compare modes are mutually exclusive.\n\
+Alternatively the entire header or specific fields can be printed.\n\
 \n\
 Options:\n\
-	-a, --abbrev: Print the abbreviated header (default).\n\
-	-f, --full:   Print the entire header.\n\
-	-c, --comp:   Compare first file to all others and print message if the.\n\
-	              physical spaces are different.\n\
-	-h, --help:   Print this message and quit.\n\
+	-a, --abbrev : Print the abbreviated header (default).\n\
+	-f, --full   : Print the entire header.\n\
+	-h, --help   : Print this message and quit.\n\
+	--voxdims    : Print the voxel dimensions.\n\
+	--tr         : Print the TR.\n\
+	--xform      : Print the highest priority transform.\n\
 ";
 
-enum class Mode { Abbrev, Full, Cmp };
 
-static Mode mode = Mode::Abbrev;
-
+static int printAbbrev = false, printFull = false,
+           printVoxdims = false, printTR = false, printXform = false;
 static struct option long_options[] = {
-	{"abbrev", no_argument, 0, 'a'},
-	{"full",   no_argument, 0, 'f'},
-	{"comp",   no_argument, 0, 'c'},
-	{"help",   no_argument, 0, 'h'},
+	{"abbrev",  no_argument, 0, 'a'},
+	{"full",    no_argument, 0, 'f'},
+	{"help",    no_argument, 0, 'h'},
+	{"voxdims", no_argument, &printVoxdims, 1},
+	{"tr",      no_argument, &printTR, 1},
+	{"xform",   no_argument, &printXform, 1},
 	{0, 0, 0, 0}
 };
 
@@ -45,9 +47,8 @@ int main(int argc, char **argv) {
 	int indexptr = 0, c;
 	while ((c = getopt_long(argc, argv, "afch", long_options, &indexptr)) != -1) {
 		switch (c) {
-			case 'a': mode = Mode::Abbrev; break;
-			case 'f': mode = Mode::Full; break;
-			case 'c': mode = Mode::Cmp; break;
+			case 'a': printAbbrev = true; printFull = false; break;
+			case 'f': printFull = true; printAbbrev = false; break;
 			case '?': // getopt will print an error message
 			case 'h':
 				cout << usage << endl;
@@ -58,6 +59,8 @@ int main(int argc, char **argv) {
 		cerr << "No input image file specified." << endl;
 		cout << usage << endl;
 		return EXIT_FAILURE;
+	} else if (optind == 1) { // No other arguments specified
+		printAbbrev = true;
 	}
 	vector<Nifti::File> images;
 	try {
@@ -69,32 +72,30 @@ int main(int argc, char **argv) {
 		cerr << e.what() << endl;
 	}
 
-	size_t did_not_match = 0;
-	if (mode == Mode::Cmp) { // Compare first image to all others and check headers are compatible
-		cout << "Checking headers against: " << images[0].headerPath() << endl;
-		for (auto im = images.begin() + 1; im != images.end(); im++) {
-			if (!images[0].header().matchesSpace(im->header())) {
-				cout << im->headerPath() << " does not match." << endl;
-				did_not_match++;
-			}
-		}
-		cout << did_not_match << " headers did not match." << endl;
-	}
-	
 	for (auto& im: images) {
 		Nifti::Header hdr = im.header();
-		if (mode == Mode::Abbrev) {
+		if (printAbbrev) {
 			cout << "Image path: " << im.imagePath() << endl;
 			cout << "Datatype:   " << hdr.typeInfo().name << endl;
 			cout << "Dimensions: " << hdr.dims().transpose() << " (rank " << to_string(hdr.rank()) << ")" << endl;
 			cout << "Voxel size: " << hdr.voxDims().transpose() << endl;
 			cout << "XForm: " << endl << hdr.transform().matrix() << endl;
 			(im.extensions().size() > 0) ? cout << "Has extensions." << endl : cout << "No extensions." << endl;
-		} else if (mode == Mode::Full) {
+		}
+		if (printFull) {
 			cout << "Full Nifti Header for file: " << im.imagePath() << endl;
 			cout << hdr << endl;
 			cout << "Number of extensions: " << im.extensions().size() << endl;
 		}
+		if (printVoxdims) {
+			if (hdr.rank() > 3) {
+				cout << hdr.voxDims().head(3).transpose() << endl;
+			} else {
+				cout << hdr.voxDims() << endl;
+			}
+		}
+		if (printTR) { cout << hdr.voxDim(4) << endl; }
+		if (printXform) { cout << hdr.transform().matrix() << endl; }
 	}
 	
 	return EXIT_SUCCESS;
