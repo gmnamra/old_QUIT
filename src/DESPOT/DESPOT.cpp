d4d5aa97380de528abc5be9db541bc05da93bc71
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-02-17 20:38:20 +0000

    Removed old code and cleaned up structure.

--- src/DESPOT/DESPOT.cpp
+++ src/DESPOT/DESPOT.cpp
@@ -32,114 +32,6 @@ double clamp(double value, double low, double high)
 }
 
 //******************************************************************************
-// Basic least squares fitting
-//******************************************************************************
-void linearLeastSquares(const ArrayXd &X, const ArrayXd &Y,
-                        double &slope, double &inter, double &res)
-{
-	eigen_assert(X.size() == Y.size());
-	double sumX, sumY, sumXX, sumXY;
-	sumX = sumY = sumXX = sumXY = 0.;
-	for (int i = 0; i < X.size(); i++) {
-		double x = X[i];
-		double y = Y[i];
-		
-		sumX  += x;
-		sumY  += y;
-		sumXX += (x*x);
-		sumXY += (x*y);
-	}
-	
-	slope = (sumXY - (sumX * sumY) / X.size()) /
-	        (sumXX - (sumX * sumX) / X.size());
-	inter = (sumY - (slope) * sumX) / X.size();
-	
-	res = (Y - (X*slope + inter)).square().sum();
-}
-
-double classicDESPOT1(const ArrayXd &flipAngles, const ArrayXd &spgrVals,
-				      double TR, double B1, double &M0, double &T1)
-{
-	// Linearise the data, then least-squares
-	long n = flipAngles.size();
-	ArrayXd X(n), Y(n);
-	double slope, inter, res;
-	for (long i = 0; i < flipAngles.size(); i++) {
-		X[i] = spgrVals[i] / tan(flipAngles[i] * B1);
-		Y[i] = spgrVals[i] / sin(flipAngles[i] * B1);
-	}
-	linearLeastSquares(X, Y, slope, inter, res);	
-	T1 = -TR / log(slope);
-	M0 = inter / (1. - slope);
-	return res;
-}
-
-double classicDESPOT2(const ArrayXd &flipAngles, const ArrayXd &ssfpVals,
-                      double TR, double T1, double B1, double &M0, double &T2) {
-	// As above, linearise, then least-squares
-	long n = flipAngles.size();
-	ArrayXd X(n), Y(n);
-	double slope, inter, residual;
-	for (long i = 0; i < flipAngles.size(); i++) {
-		X[i] = ssfpVals[i] / tan(flipAngles[i] * B1);
-		Y[i] = ssfpVals[i] / sin(flipAngles[i] * B1);
-	}
-	linearLeastSquares(X, Y, slope, inter, residual);
-	double eT1 = exp(-TR / T1);
-	T2 = -TR / log((eT1 - slope) / (1. - slope * eT1));
-	double eT2 = exp(-TR / T2);
-	M0 = inter * (1. - eT1 * eT2) / (eT2 * (1. - eT1));
-	return residual;
-}
-
-ArrayXd SPGR(const ArrayXd &flip, const double &TR, const double &B1, const double &M0, const double &T1)
-{
-	double e1 = exp(-TR / T1);
-	ArrayXd spgr = M0 * (1. - e1) * sin(B1 * flip) / (1. - (e1 * cos(B1 * flip)));
-	return spgr;
-}
-
-ArrayXd IRSPGR(const ArrayXd &TI, const double &TR, const double &B1,
-               const double &flip, const double &eff,
-			   const double &M0, const double &T1)
-{
-	double irEfficiency;
-	// Adiabatic pulses aren't susceptible to B1 problems.
-	if (eff > 0)
-		irEfficiency = cos(eff * M_PI) - 1;
-	else
-		irEfficiency = cos(B1 * M_PI) - 1;	
-	
-	ArrayXd eTI = exp(-TI / T1);
-	ArrayXd eFull = exp(-(TI + TR) / T1);
-
-	ArrayXd irspgr = ((M0 * sin(B1 * flip) * (1. + irEfficiency * eTI + eFull))).abs();
-	return irspgr;
-}
-
-MagVector MP_RAGE(const ArrayXd &TI, const double T1, const double TD,
-                  const double TR, const int N, const double alpha) {
-	const double M0 = 1.;
-	const double T1s = 1. / (1./T1 - log(cos(alpha))/TR);
-	const double M0s = M0 * (1. - exp(-TR/T1)) / (1 - exp(-TR/T1s));
-
-	const double A1 = M0s*(1 - exp(-(N*TR)/T1s));
-	const double A2 = M0*(1 - exp(-TD/T1));
-	const ArrayXd A3 = M0*(1 - exp(-TI/T1));
-	const double B1 = exp(-(N*TR)/T1s);
-	const double B2 = exp(-TD/T1);
-	const ArrayXd B3 = -exp(-TI/T1);
-
-	const ArrayXd A = A3 + A2*B3 + A1*B2*B3;
-	const ArrayXd B = B1*B2*B3;
-	const ArrayXd M1 = A / (1. - B);
-
-	MagVector M(3, TI.size()); M.setZero();
-	M.row(0) = M1 * sin(alpha);
-	return M;
-}
-
-//******************************************************************************
 #pragma mark Magnetisation Evolution Matrices, helper functions etc.
 //******************************************************************************
 typedef Matrix<double, 6, 6> Matrix6d;
@@ -297,6 +189,28 @@ MagVector One_SSFP_Ellipse(const ArrayXd &flip, cdbl TR, cdbl PD, cdbl T1, cdbl 
     result.row(1) = M * sin(theta);
 
     return result;
+}
+
+MagVector MP_RAGE(const ArrayXd &TI, const double T1, const double TD,
+                  const double TR, const int N, const double alpha) {
+	const double M0 = 1.;
+	const double T1s = 1. / (1./T1 - log(cos(alpha))/TR);
+	const double M0s = M0 * (1. - exp(-TR/T1)) / (1 - exp(-TR/T1s));
+
+	const double A1 = M0s*(1 - exp(-(N*TR)/T1s));
+	const double A2 = M0*(1 - exp(-TD/T1));
+	const ArrayXd A3 = M0*(1 - exp(-TI/T1));
+	const double B1 = exp(-(N*TR)/T1s);
+	const double B2 = exp(-TD/T1);
+	const ArrayXd B3 = -exp(-TI/T1);
+
+	const ArrayXd A = A3 + A2*B3 + A1*B2*B3;
+	const ArrayXd B = B1*B2*B3;
+	const ArrayXd M1 = A / (1. - B);
+
+	MagVector M(3, TI.size()); M.setZero();
+	M.row(0) = M1 * sin(alpha);
+	return M;
 }
 
 //******************************************************************************
--- src/DESPOT/DESPOT.h
+++ src/DESPOT/DESPOT.h
@@ -22,28 +22,12 @@ using namespace std;
 using namespace Eigen;
 
 //******************************************************************************
-#pragma mark Convenience stuff
+// Convenience
 //******************************************************************************
 double clamp(double value, double low, double high);
 
-void linearLeastSquares(const ArrayXd &X, const ArrayXd &Y,
-						double &slope, double &inter, double &res);
-
-
-double classicDESPOT1(const ArrayXd &flipAngles, const ArrayXd &spgrVals,
-				      double TR, double B1, double &M0, double &T1);
-double classicDESPOT2(const ArrayXd &flipAngles, const ArrayXd &ssfpVals,
-                      double TR, double T1, double B1, double &M0, double &T2);
-
 //******************************************************************************
-#pragma mark Basic Signal Equations
-//******************************************************************************
-ArrayXd SPGR(const ArrayXd &flip, const double &TR, const double &B1, const double &M0, const double &T1);
-ArrayXd IRSPGR(const ArrayXd &TI, const double &TR, const double &B1,
-               const double &flip, const double &eff,
-               const double &M0, const double &T1);
-//******************************************************************************
-#pragma Magnetisation Evolution Matrices, helper functions etc.
+// Magnetisation Evolution Matrices, helper functions etc.
 //******************************************************************************
 typedef const double cdbl; // To save tedious typing
 
@@ -65,8 +49,7 @@ inline const Matrix6d Exchange(cdbl &k_ab, cdbl &k_ba);
 const void CalcExchange(cdbl tau_a, cdbl f_a, cdbl f_b, double &k_ab, double &k_ba);
 
 //******************************************************************************
-#pragma mark Actual Signal Equations
-// Parameters are { T1, T2, f0 }
+// Actual Signal Equations
 //******************************************************************************
 MagVector One_SPGR(const ArrayXd &flip, cdbl TR, cdbl PD, cdbl T1);
 MagVector One_SSFP(const ArrayXd &flip, cdbl TR, cdbl ph, cdbl PD, cdbl T1, cdbl T2, cdbl f0);
@@ -74,17 +57,13 @@ MagVector One_SSFP_Finite(const ArrayXd &flip, const bool spoil, cdbl TR, cdbl T
                           cdbl PD, cdbl T1, cdbl T2, cdbl f0);
 MagVector One_SSFP_Ellipse(const ArrayXd &flip, cdbl TR, cdbl PD, cdbl T1, cdbl T2, cdbl f0);
 MagVector MP_RAGE(const ArrayXd &TI, cdbl T1, cdbl TD, cdbl TR, const int N, cdbl alpha);
-//******************************************************************************
-// Parameters are { T1_a, T2_a, T1_b, T2_b, tau_a, f_a, f0 }
-//******************************************************************************
+
 MagVector Two_SPGR(const ArrayXd &flip, cdbl TR, cdbl PD, cdbl T1_a, cdbl T1_b, cdbl tau_a, cdbl f_a);
 MagVector Two_SSFP(const ArrayXd &flip, cdbl TR, cdbl ph, cdbl PD, cdbl T1_a, cdbl T2_a, cdbl T1_b, cdbl T2_b, cdbl tau_a, cdbl f_a, cdbl f0);
 MagVector Two_SSFP_Finite(const ArrayXd &flip, const bool spoil, cdbl TR, cdbl Trf, cdbl TE, cdbl ph,
                           cdbl PD, cdbl T1_a, cdbl T2_a, cdbl T1_b, cdbl T2_b,
-						  cdbl tau_a, cdbl f_a, cdbl f0);
-//******************************************************************************
-// Parameters are { T1a, T2a, T1b, T2b, T1c, T2c, tau_a, f_a, f_c, f0 }
-//******************************************************************************
+                          cdbl tau_a, cdbl f_a, cdbl f0);
+
 MagVector Three_SPGR(const ArrayXd &flip, cdbl TR, cdbl PD, cdbl T1_a, cdbl T1_b, cdbl T1_c, cdbl tau_a, cdbl f_a, cdbl f_c);
 MagVector Three_SSFP(const ArrayXd &flip, cdbl TR, cdbl ph, cdbl PD, cdbl T1_a, cdbl T2_a, cdbl T1_b, cdbl T2_b, cdbl T1_c, cdbl T2_c, cdbl tau_a, cdbl f_a, cdbl f_c, cdbl f0);
 MagVector Three_SSFP_Finite(const ArrayXd &flip, const bool spoil, cdbl TR, cdbl Trf, cdbl TE, cdbl ph,
