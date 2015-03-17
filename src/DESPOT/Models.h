bada78f9bddbc8df320e7bccf0d166ed8aadfd66
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-03-17 17:24:45 +0000

    Updated typedefs and removed extraneous consts.

--- src/DESPOT/Models.h
+++ src/DESPOT/Models.h
@@ -24,7 +24,7 @@ using namespace std;
 using namespace Eigen;
 
 //cdbl and carr already typedef'd in SignalEquations
-typedef const VectorXd cvec;
+typedef const VectorXd cvecd;
 
 enum class FieldStrength { Three, Seven, User };
 static const string to_string(const FieldStrength& f);
@@ -33,23 +33,23 @@ class Model {
 public:
 	virtual string Name() const = 0;
 	virtual size_t nParameters() const = 0;
-	virtual bool ValidParameters(cvec &params) const = 0;
+	virtual bool ValidParameters(cvecd &params) const = 0;
 	virtual const vector<string> &Names() const = 0;
 	virtual ArrayXXd Bounds(const FieldStrength f, cdbl TR) const = 0;
 
-	virtual VectorXcd SPGR(cvec &params, carr &a, cdbl TR) const;
-	virtual VectorXcd SPGRFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const;
-	virtual VectorXcd MPRAGE(cvec &params, cdbl a, cdbl TR, const int N, cvec &TI, cdbl TD) const;
-	virtual VectorXcd SSFP(cvec &params, carr &a, cdbl TR, cdbl phi) const;
-	virtual VectorXcd SSFPEllipse(cvec &params, carr &a, cdbl TR) const;
-	virtual VectorXcd SSFPFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const;
+	virtual VectorXcd SPGR(cvecd &params, carr &a, cdbl TR) const;
+	virtual VectorXcd SPGRFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const;
+	virtual VectorXcd MPRAGE(cvecd &params, cdbl a, cdbl TR, const int N, cvecd &TI, cdbl TD) const;
+	virtual VectorXcd SSFP(cvecd &params, carr &a, cdbl TR, cdbl phi) const;
+	virtual VectorXcd SSFPEllipse(cvecd &params, carr &a, cdbl TR) const;
+	virtual VectorXcd SSFPFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const;
 };
 
 #define DECLARE_INTERFACE( )\
 public:\
 	string Name() const override;\
 	size_t nParameters() const override;\
-	bool ValidParameters(cvec &p) const override;\
+	bool ValidParameters(cvecd &p) const override;\
 	const vector<string> &Names() const override;\
 	ArrayXXd Bounds(const FieldStrength f, cdbl TR) const override;\
 
@@ -57,39 +57,39 @@ public:\
 class SCD : public Model {
 	DECLARE_INTERFACE()
 
-	virtual VectorXcd SPGR(cvec &params, carr &a, cdbl TR) const override;
-	virtual VectorXcd SPGRFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const override;
-	virtual VectorXcd MPRAGE(cvec &params, cdbl a, cdbl TR, const int N, cvec &TI, cdbl TD) const override;
-	virtual VectorXcd SSFP(cvec &params, carr &a, cdbl TR, cdbl phi) const override;
-	virtual VectorXcd SSFPFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const override;
-	virtual VectorXcd SSFPEllipse(cvec &params, carr &a, cdbl TR) const;
+	virtual VectorXcd SPGR(cvecd &params, carr &a, cdbl TR) const override;
+	virtual VectorXcd SPGRFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const override;
+	virtual VectorXcd MPRAGE(cvecd &params, cdbl a, cdbl TR, const int N, cvecd &TI, cdbl TD) const override;
+	virtual VectorXcd SSFP(cvecd &params, carr &a, cdbl TR, cdbl phi) const override;
+	virtual VectorXcd SSFPFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const override;
+	virtual VectorXcd SSFPEllipse(cvecd &params, carr &a, cdbl TR) const;
 };
 
 class MCD2 : public Model {
 	DECLARE_INTERFACE()
 
-	virtual VectorXcd SPGR(cvec &params, carr &a, cdbl TR) const override;
-	virtual VectorXcd SPGRFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const override;
-	virtual VectorXcd SSFP(cvec &params, carr &a, cdbl TR, cdbl phi) const override;
-	virtual VectorXcd SSFPFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const override;
+	virtual VectorXcd SPGR(cvecd &params, carr &a, cdbl TR) const override;
+	virtual VectorXcd SPGRFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const override;
+	virtual VectorXcd SSFP(cvecd &params, carr &a, cdbl TR, cdbl phi) const override;
+	virtual VectorXcd SSFPFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const override;
 };
 
 class MCD3 : public Model {
 	DECLARE_INTERFACE()
 
-	virtual VectorXcd SPGR(cvec &params, carr &a, cdbl TR) const override;
-	virtual VectorXcd SPGRFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const override;
-	virtual VectorXcd SSFP(cvec &params, carr &a, cdbl TR, cdbl phi) const override;
-	virtual VectorXcd SSFPFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const override;
+	virtual VectorXcd SPGR(cvecd &params, carr &a, cdbl TR) const override;
+	virtual VectorXcd SPGRFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const override;
+	virtual VectorXcd SSFP(cvecd &params, carr &a, cdbl TR, cdbl phi) const override;
+	virtual VectorXcd SSFPFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const override;
 };
 
 /*class MCD3_2OM : public Model {
 	DECLARE_INTERFACE()
 
-	virtual VectorXcd SPGR(cvec &params, carr &a, cdbl TR) const override;
-	virtual VectorXcd SPGRFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const override;
-	virtual VectorXcd SSFP(cvec &params, carr &a, cdbl TR, cdbl phi) const override;
-	virtual VectorXcd SSFPFinite(cvec &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const override;
+	virtual VectorXcd SPGR(cvecd &params, carr &a, cdbl TR) const override;
+	virtual VectorXcd SPGRFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl TE) const override;
+	virtual VectorXcd SSFP(cvecd &params, carr &a, cdbl TR, cdbl phi) const override;
+	virtual VectorXcd SSFPFinite(cvecd &params, carr &a, cdbl TR, cdbl T_rf, cdbl phi) const override;
 };*/
 
 #undef DECLARE_INTERFACE
--- src/DESPOT/SignalEquations.h
+++ src/DESPOT/SignalEquations.h
@@ -30,7 +30,7 @@ double clamp(double value, double low, double high);
 // Magnetisation Evolution Matrices, helper functions etc.
 //******************************************************************************
 typedef const double cdbl; // To save tedious typing
-typedef const ArrayXd carr;
+typedef const ArrayXd carrd;
 
 typedef Matrix<double, 6, 6> Matrix6d;
 typedef Matrix<double, 6, 1> Vector6d;
@@ -38,9 +38,9 @@ typedef Matrix<double, 9, 9> Matrix9d;
 typedef Matrix<double, 9, 1> Vector9d;
 typedef Matrix<double, 3, Dynamic> MagVector;
 
-const VectorXd SigMag(const MagVector &M_in);
-const VectorXcd SigComplex(const MagVector &M_in);
-const MagVector SumMC(const MatrixXd &M_in);
+VectorXd SigMag(const MagVector &M_in);
+VectorXcd SigComplex(const MagVector &M_in);
+MagVector SumMC(const MatrixXd &M_in);
 
 inline const Matrix3d Relax(cdbl &T1, cdbl &T2);
 inline const Matrix3d InfinitesimalRF(cdbl &dalpha);
@@ -52,22 +52,22 @@ const void CalcExchange(cdbl tau_a, cdbl f_a, cdbl f_b, double &k_ab, double &k_
 //******************************************************************************
 // Actual Signal Equations
 //******************************************************************************
-MagVector One_SPGR(carr &flip, cdbl TR, cdbl PD, cdbl T1);
-MagVector One_SSFP(carr &flip, cdbl TR, cdbl ph, cdbl PD, cdbl T1, cdbl T2, cdbl f0);
-MagVector One_SSFP_Finite(carr &flip, const bool spoil, cdbl TR, cdbl Trf, cdbl TE, cdbl ph,
+MagVector One_SPGR(carrd &flip, cdbl TR, cdbl PD, cdbl T1);
+MagVector One_SSFP(carrd &flip, cdbl TR, cdbl ph, cdbl PD, cdbl T1, cdbl T2, cdbl f0);
+MagVector One_SSFP_Finite(carrd &flip, const bool spoil, cdbl TR, cdbl Trf, cdbl TE, cdbl ph,
                           cdbl PD, cdbl T1, cdbl T2, cdbl f0);
-MagVector One_SSFP_Ellipse(carr &flip, cdbl TR, cdbl PD, cdbl T1, cdbl T2, cdbl f0);
-MagVector MP_RAGE(cdbl flip, cdbl TR, const int N, carr &TI, cdbl TD, cdbl PD, cdbl T1);
+MagVector One_SSFP_Ellipse(carrd &flip, cdbl TR, cdbl PD, cdbl T1, cdbl T2, cdbl f0);
+MagVector MP_RAGE(cdbl flip, cdbl TR, const int N, carrd &TI, cdbl TD, cdbl PD, cdbl T1);
 
-MagVector Two_SPGR(carr &flip, cdbl TR, cdbl PD, cdbl T1_a, cdbl T1_b, cdbl tau_a, cdbl f_a);
-MagVector Two_SSFP(carr &flip, cdbl TR, cdbl ph, cdbl PD, cdbl T1_a, cdbl T2_a, cdbl T1_b, cdbl T2_b, cdbl tau_a, cdbl f_a, cdbl f0_a, cdbl f0_b);
-MagVector Two_SSFP_Finite(carr &flip, const bool spoil, cdbl TR, cdbl Trf, cdbl TE, cdbl ph,
+MagVector Two_SPGR(carrd &flip, cdbl TR, cdbl PD, cdbl T1_a, cdbl T1_b, cdbl tau_a, cdbl f_a);
+MagVector Two_SSFP(carrd &flip, cdbl TR, cdbl ph, cdbl PD, cdbl T1_a, cdbl T2_a, cdbl T1_b, cdbl T2_b, cdbl tau_a, cdbl f_a, cdbl f0_a, cdbl f0_b);
+MagVector Two_SSFP_Finite(carrd &flip, const bool spoil, cdbl TR, cdbl Trf, cdbl TE, cdbl ph,
                           cdbl PD, cdbl T1_a, cdbl T2_a, cdbl T1_b, cdbl T2_b,
                           cdbl tau_a, cdbl f_a, cdbl f0_a, cdbl f0_b);
 
-MagVector Three_SPGR(carr &flip, cdbl TR, cdbl PD, cdbl T1_a, cdbl T1_b, cdbl T1_c, cdbl tau_a, cdbl f_a, cdbl f_c);
-MagVector Three_SSFP(carr &flip, cdbl TR, cdbl ph, cdbl PD, cdbl T1_a, cdbl T2_a, cdbl T1_b, cdbl T2_b, cdbl T1_c, cdbl T2_c, cdbl tau_a, cdbl f_a, cdbl f_c, cdbl f0_a, cdbl f0_b, cdbl f0_c);
-MagVector Three_SSFP_Finite(carr &flip, const bool spoil, cdbl TR, cdbl Trf, cdbl TE, cdbl ph,
+MagVector Three_SPGR(carrd &flip, cdbl TR, cdbl PD, cdbl T1_a, cdbl T1_b, cdbl T1_c, cdbl tau_a, cdbl f_a, cdbl f_c);
+MagVector Three_SSFP(carrd &flip, cdbl TR, cdbl ph, cdbl PD, cdbl T1_a, cdbl T2_a, cdbl T1_b, cdbl T2_b, cdbl T1_c, cdbl T2_c, cdbl tau_a, cdbl f_a, cdbl f_c, cdbl f0_a, cdbl f0_b, cdbl f0_c);
+MagVector Three_SSFP_Finite(carrd &flip, const bool spoil, cdbl TR, cdbl Trf, cdbl TE, cdbl ph,
                             cdbl PD, cdbl T1_a, cdbl T2_a, cdbl T1_b, cdbl T2_b, cdbl T1_c, cdbl T2_c,
                             cdbl tau_a, cdbl f_a, cdbl f_c,
                             cdbl f0_a, cdbl f0_b, cdbl f0_c);
