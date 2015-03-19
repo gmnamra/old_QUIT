c48af0b8bc16c473bb7c274f140d4ffac341ca09
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-03-19 17:42:56 +0000

    Created basic single component SPGR and SSFP signals.

--- tests/test_despot.sh
+++ tests/test_despot.sh
@@ -15,10 +15,30 @@ cd $DATADIR
 
 echo "Starting DESPOT tests"
 
-DIMS="16 16 16"
-run_test "Create T1 volume" $QUITDIR/niicreate -d "$DIMS" -f 1 T1.nii
-run_test "Create T2 volume" $QUITDIR/niicreate -d "$DIMS" -g "2 0.05 0.5" T2.nii
+DIMS="32 32 32"
+run_test "Create T1 volume" $QUITDIR/niicreate -d "$DIMS" -g "0 0.5 5" T1.nii
+run_test "Create T2 volume" $QUITDIR/niicreate -d "$DIMS" -g "1 0.05 0.5" T2.nii
 run_test "Create PD volume" $QUITDIR/niicreate -d "$DIMS" -f 1 PD.nii
+run_test "Create f0 volume" $QUITDIR/niicreate -d "$DIMS" -g "2 -50.0 50.0" f0.nii
 
-run_test "Create SPGR data" $QUITDIR/mcsignal
+# Create input for mcsignal
+INPUT="PD.nii
+T1.nii
+T2.nii
+f0.nii
+SPGR
+spgr.nii
+3
+5 10 15
+0.01
+SSFP
+ssfp.nii
+3
+15 30 45
+4
+0 90 180 270
+0.005
+END"
+echo "$INPUT" > mcsignal.in
+run_test "Create SPGR data" $QUITDIR/mcsignal --1 < mcsignal.in
 cd ..
