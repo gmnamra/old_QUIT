a11f04c51e1182613873c8a32c9b9964b9e83d12
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-03-19 20:34:34 +0000

    Added comparison tests using FSL.

--- tests/test_despot.sh
+++ tests/test_despot.sh
@@ -21,24 +21,45 @@ run_test "Create T2 volume" $QUITDIR/niicreate -d "$DIMS" -g "1 0.05 0.5" T2.nii
 run_test "Create PD volume" $QUITDIR/niicreate -d "$DIMS" -f 1 PD.nii
 run_test "Create f0 volume" $QUITDIR/niicreate -d "$DIMS" -g "2 -50.0 50.0" f0.nii
 
-# Create input for mcsignal
-INPUT="PD.nii
+# Setup parameters
+SPGR_FILE="spgr.nii"
+SPGR_PAR="3
+5 10 15
+0.01"
+SSFP_FILE="ssfp.nii"
+SSFP_PAR="3
+15 30 45
+4
+0 90 180 270
+0.05"
+
+# Create input for Single Component
+MCSIG_INPUT="PD.nii
 T1.nii
 T2.nii
 f0.nii
 SPGR
-spgr.nii
-3
-5 10 15
-0.01
+$SPGR_FILE
+$SPGR_PAR
 SSFP
-ssfp.nii
-3
-15 30 45
-4
-0 90 180 270
-0.005
+$SSFP_FILE
+$SSFP_PAR
 END"
-echo "$INPUT" > mcsignal.in
+echo "$MCSIG_INPUT" > mcsignal.in
 run_test "Create SPGR data" $QUITDIR/mcsignal --1 < mcsignal.in
+
+echo "$SPGR_PAR" > despot1.in
+run_test "DESPOT1" $QUITDIR/despot1 $SPGR_FILE < despot1.in
+
+run_test "SSFPBANDS" $QUITDIR/ssfpbands -2 $SSFP_FILE
+
+D2E_PAR="3
+15 30 45
+0.05"
+echo "$D2E_PAR" > despot2.in
+run_test "DESPOT2GS" $QUITDIR/despot2 -e D1_T1.nii ${SSFP_FILE%.nii}_lreg_2p.nii < despot2.in
+
+compare_test "DESPOT1" T1.nii D1_T1.nii 0.01
+compare_test "DESPOT2E" T2.nii D2_T2.nii 0.02
+
 cd ..
