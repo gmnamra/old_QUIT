ff2e7ad2251fd34f1cd4200d23473bbec1fe5615
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-03-24 21:00:50 +0000

    Added hifi tests.

--- tests/test_scdespot.sh
+++ tests/test_scdespot.sh
@@ -31,6 +31,13 @@ SSFP_PAR="3
 4
 0 90 180 270
 0.05"
+MPRAGE_FILE="mprage.nii"
+MPRAGE_PAR="5
+0.01
+11
+1
+0.5
+0"
 
 # Create input for Single Component
 MCSIG_INPUT="PD.nii
@@ -43,11 +50,16 @@ $SPGR_FILE
 SSFP
 $SSFP_PAR
 $SSFP_FILE
+MPRAGE
+$MPRAGE_PAR
+$MPRAGE_FILE
 END"
 echo "$MCSIG_INPUT" > mcsignal.in
 run_test "CREATE_SIGNALS" $QUITDIR/mcsignal --1 < mcsignal.in
 
 echo "$SPGR_PAR" > despot1.in
+echo "$SPGR_PAR
+$MPRAGE_PAR" > despot1hifi.in
 echo "$SSFP_PAR" > despot2fm.in
 D2GS_PAR="3
 15 30 45
@@ -55,10 +67,12 @@ D2GS_PAR="3
 echo "$D2GS_PAR" > despot2gs.in
 
 run_test "DESPOT1" $QUITDIR/despot1 $SPGR_FILE -n < despot1.in
+run_test "DESPOT1HIFI" $QUITDIR/despot1hifi $SPGR_FILE $MPRAGE_FILE -n -v -N < despot1hifi.in
 run_test "SSFPBANDS" $QUITDIR/ssfpbands -2 $SSFP_FILE
 run_test "DESPOT2FM" $QUITDIR/despot2fm D1_T1.nii $SSFP_FILE -n -fASYM -v < despot2fm.in
 run_test "DESPOT2GS" $QUITDIR/despot2 -e D1_T1.nii ${SSFP_FILE%.nii}_lreg_2p.nii -n < despot2gs.in
 compare_test "DESPOT1" T1.nii D1_T1.nii 0.01
+compare_test "HIFI_T1" T1.nii HIFI_T1.nii 0.01
 compare_test "DESPOT2FM" T2.nii FM_T2.nii 0.05
 compare_test "DESPOT2GS" T2.nii D2_T2.nii 0.05
 
