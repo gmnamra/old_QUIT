579a6383666ed8a7e3a18394ea03b1acd21f49c9
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-03-24 20:46:23 +0000

    Implemented ARLO (Pei 2015) method. Implemented multi-volume fitting (not tested).

--- src/DESPOT/t2star.cpp
+++ src/DESPOT/t2star.cpp
@@ -38,12 +38,18 @@ Options:\n\
 	--no-prompt, -n   : Suppress input prompts\n\
 	--out, -o path    : Add a prefix to the output filenames\n\
 	--mask, -m file   : Mask input with specified file\n\
+	--echos, -e N     : Number of echos (timeseries input)\n\
 	--thresh, -t n    : Threshold maps at PD < n\n\
 	--clamp, -c n     : Clamp T2* between 0 and n\n\
+	--algo, -a L      : LLS algorithm (default)\n\
+	           A      : ARLO algorithm\n\
 	--resids, -r      : Write out per flip-angle residuals\n\
 	--threads, -T N   : Use N threads (default=hardware limit)\n"
 };
 
+enum class Algo { LogLin, ARLO };
+static Algo algo = Algo::LogLin;
+static int NE = 0;
 static bool verbose = false, prompt = true, all_residuals = false;
 static string outPrefix;
 static double thresh = -numeric_limits<double>::infinity();
@@ -55,13 +61,15 @@ static struct option long_options[] =
 	{"no-prompt", no_argument, 0, 'n'},
 	{"out", required_argument, 0, 'o'},
 	{"mask", required_argument, 0, 'm'},
+	{"echos", required_argument, 0, 'e'},
 	{"thresh", required_argument, 0, 't'},
 	{"clamp", required_argument, 0, 'c'},
+	{"algo", required_argument, 0, 'a'},
 	{"threads", required_argument, 0, 'T'},
 	{"resids", no_argument, 0, 'r'},
 	{0, 0, 0, 0}
 };
-static const char *short_opts = "hvnm:o:b:t:c:T:r";
+static const char *short_opts = "hvnm:e:o:b:t:c:a:T:r";
 //******************************************************************************
 // Main
 //******************************************************************************
@@ -87,11 +95,21 @@ int main(int argc, char **argv) {
 				outPrefix = optarg;
 				cout << "Output prefix will be: " << outPrefix << endl;
 				break;
+			case 'e': NE = atoi(optarg); break;
 			case 't': thresh = atof(optarg); break;
 			case 'c':
 				clamp_lo = 0;
 				clamp_hi = atof(optarg);
 				break;
+			case 'a':
+				switch (*optarg) {
+					case 'L': algo = Algo::LogLin; if (verbose) cout << "LogLin algorithm selected." << endl; break;
+					case 'A': algo = Algo::ARLO; if (verbose) cout << "ARLO algorithm selected." << endl; break;
+					default:
+						cout << "Unknown algorithm type " << optarg << endl;
+						return EXIT_FAILURE;
+						break;
+				} break;
 			case 'T':
 				threads.resize(atoi(optarg));
 				break;
@@ -120,13 +138,24 @@ int main(int argc, char **argv) {
 		if (prompt) cout << "Enter echo spacing: " << flush;
 		QUIT::Read<double>::FromLine(cin, ESP);
 	}
+	if (NE < 1) {
+		NE = inputFile.dim(4);
+	} else {
+		// Check that NE makes sense
+		if ((inputFile.dim(4) % NE) != 0) {
+			throw(runtime_error("Number of volumes is not a multiple of NE."));
+		}
+	}
+	int NVols = inputFile.dim(4) / NE;
+
 	// Set up echo times array
-	MatrixXd X(inputFile.dim(4), 2);
+	MatrixXd X(NE, 2);
 	X(0, 0) = TE1;
 	for (int i = 1; i < X.rows(); i++) {
 		X(i, 0) = X(i-1, 0) + ESP;
 	}
 	X.col(1).setOnes();
+
 	if (verbose) {
 		cout << "Ouput prefix will be: " << outPrefix << endl;
 		cout << "Echo times are: " << X.col(0).transpose() << endl;
@@ -141,7 +170,7 @@ int main(int argc, char **argv) {
 	// Do the fitting
 	//**************************************************************************
 	const auto dims = inputFile.matrix();
-	MultiArray<float, 3> T2starVol(dims), PDVol(dims), ResVol(dims);
+	MultiArray<float, 4> T2starVol(dims, NVols), PDVol(dims, NVols), ResVol(dims, NVols);
 	MultiArray<float, 4> ResidsVols;
 	if (all_residuals) {
 		ResidsVols = MultiArray<float, 4>(dims, inputFile.dim(4));
@@ -152,28 +181,46 @@ int main(int argc, char **argv) {
 		loopStart = clock();
 		atomic<int> voxCount{0};
 		function<void (const size_t, const size_t)> process = [&] (const size_t i, const size_t j) {
-			const MultiArray<float, 3>::Index idx{i,j,k};
-			if (!maskFile || (maskVol[idx])) {
+			if (!maskFile || (maskVol[{i,j,k}])) {
 				voxCount++;
-				double T2star, PD;
-				ArrayXd signal = inputVols.slice<1>({i,j,k,0},{0,0,0,-1}).asArray().abs().cast<double>();
-				VectorXd Y = signal.log();
-				VectorXd b = (X.transpose() * X).partialPivLu().solve(X.transpose() * Y);
-				T2star = -1 / b[0];
-				PD = exp(b[1]);
-				if (PD < thresh) {
-					PD = 0.;
-					T2star = 0.;
+				for (size_t outVol = 0; outVol < NVols; outVol++) {
+					double T2star = 0, PD = 0;
+					ArrayXd signal = inputVols.slice<1>({i,j,k,outVol*NE},{0,0,0,NE}).asArray().abs().cast<double>();
+					switch (algo) {
+					case Algo::LogLin: {
+						VectorXd Y = signal.log();
+						VectorXd b = (X.transpose() * X).partialPivLu().solve(X.transpose() * Y);
+						T2star = -1 / b[0];
+						PD = exp(b[1]);
+					} break;
+					case Algo::ARLO: {
+						double si2sum = 0, sidisum = 0;
+						for (int i = 0; i < NE - 2; i++) {
+							double si = (ESP / 3) * (signal(i) + 4*signal(i+1) + signal(i+2));
+							double di = signal(i) - signal(i+2);
+							si2sum += si*si;
+							sidisum = si*di;
+						}
+						T2star = (si2sum + (ESP/3)*sidisum) / ((ESP/3)*si2sum + sidisum);
+						PD = (signal / (-X.col(0).array() / T2star).exp()).mean();
+					} break;
+					}
+
+					if (PD < thresh) {
+						PD = 0.;
+						T2star = 0.;
+					}
+					T2star = clamp(T2star, clamp_lo, clamp_hi);
+					ArrayXd theory = PD * (-X.col(0).array() / T2star).exp();
+					ArrayXd resids = (signal - theory);
+					if (all_residuals) {
+						ResidsVols.slice<1>({i,j,k,outVol*NE},{0,0,0,NE}).asArray() = resids.cast<float>();
+					}
+					const MultiArray<float, 4>::Index idx{i,j,k,outVol};
+					T2starVol[idx]  = static_cast<float>(T2star);
+					PDVol[idx]  = static_cast<float>(PD);
+					ResVol[idx] = static_cast<float>(sqrt(resids.square().sum() / resids.rows()) / PD);
 				}
-				T2star = clamp(T2star, clamp_lo, clamp_hi);
-				ArrayXd theory = PD * (-X.col(0).array() / T2star).exp();
-				ArrayXd resids = (signal - theory);
-				if (all_residuals) {
-					ResidsVols.slice<1>({i,j,k,0},{0,0,0,-1}).asArray() = resids.cast<float>();
-				}
-				T2starVol[idx]  = static_cast<float>(T2star);
-				PDVol[idx]  = static_cast<float>(PD);
-				ResVol[idx] = static_cast<float>(sqrt(resids.square().sum() / resids.rows()) / PD);
 			}
 		};
 
@@ -193,7 +240,7 @@ int main(int argc, char **argv) {
 	outPrefix = outPrefix + "ME_";
 	Nifti::Header outHdr = inputFile.header();
 	outHdr.description = version;
-	outHdr.setDim(4, 1);
+	outHdr.setDim(4, NVols);
 	outHdr.setDatatype(Nifti::DataType::FLOAT32);
 	outHdr.intent = Nifti::Intent::Estimate;
 	outHdr.intent_name = "T2* (seconds)";
