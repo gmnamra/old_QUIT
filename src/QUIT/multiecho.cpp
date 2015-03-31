c16b21c2024f762285f71fcbca9bd25b603401ce
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-03-31 22:59:04 +0100

    Removed T2star references. Updated to use MultiEcho Sequence. Added a non-linear Levenberg-Marquardt mode, but doesn't work currently (lmder1 doesn't seem to do anything)

--- src/QUIT/multiecho.cpp
+++ src/QUIT/multiecho.cpp
@@ -20,7 +20,7 @@
 
 #include "Nifti/Nifti.h"
 #include "QUIT/QUIT.h"
-#include "SignalEquations.h"
+#include "Sequence.h"
 
 using namespace std;
 using namespace Eigen;
@@ -43,13 +43,14 @@ Options:\n\
 	--clamp, -c n     : Clamp T2 between 0 and n\n\
 	--algo, -a L      : LLS algorithm (default)\n\
 	           A      : ARLO algorithm\n\
+	--its, -i N       : Max iterations for WLLS (default 4)\n\
 	--resids, -r      : Write out per flip-angle residuals\n\
 	--threads, -T N   : Use N threads (default=hardware limit)\n"
 };
 
-enum class Algo { LogLin, ARLO };
+enum class Algo { LogLin, ARLO, Nonlin };
 static Algo algo = Algo::LogLin;
-static int NE = 0;
+static int NE = 0, nIterations = 4;
 static bool verbose = false, prompt = true, all_residuals = false;
 static string outPrefix, suffix;
 static double thresh = -numeric_limits<double>::infinity();
@@ -65,11 +66,45 @@ static struct option long_options[] =
 	{"thresh", required_argument, 0, 't'},
 	{"clamp", required_argument, 0, 'c'},
 	{"algo", required_argument, 0, 'a'},
+	{"its", required_argument, 0, 'i'},
 	{"threads", required_argument, 0, 'T'},
 	{"resids", no_argument, 0, 'r'},
 	{0, 0, 0, 0}
 };
-static const char *short_opts = "hvnm:se:o:b:t:c:a:T:r";
+static const char *short_opts = "hvnm:se:o:b:t:c:a:i:T:r";
+
+class RelaxFunctor : public DenseFunctor<double> {
+	protected:
+		const SequenceBase &m_sequence;
+		const ArrayXd m_data;
+		const bool m_debug;
+		const shared_ptr<SCD> m_model;
+
+	public:
+		RelaxFunctor(SequenceBase &cs, const ArrayXd &data,
+		          const double B1, const bool debug) :
+			DenseFunctor<double>(2, cs.size()),
+			m_sequence(cs), m_data(data),
+			m_debug(debug), m_model()
+		{
+			assert(static_cast<size_t>(m_data.rows()) == values());
+		}
+
+		int operator()(const Ref<VectorXd> &params, Ref<ArrayXd> diffs) const {
+			eigen_assert(diffs.size() == values());
+			ArrayXcd s = m_sequence.signal(m_model, params, 1.0); // Fix B1 to 1.0 for now
+			diffs = s.abs() - m_data;
+			if (m_debug) {
+				cout << endl << __PRETTY_FUNCTION__ << endl;
+				cout << "p:     " << params.transpose() << endl;
+				cout << "s:     " << s.transpose() << endl;
+				cout << "data:  " << m_data.transpose() << endl;
+				cout << "diffs: " << diffs.transpose() << endl;
+			}
+			return 0;
+		}
+};
+
 //******************************************************************************
 // Main
 //******************************************************************************
@@ -96,6 +131,7 @@ int main(int argc, char **argv) {
 				cout << "Output prefix will be: " << outPrefix << endl;
 				break;
 			case 's': suffix = "star"; break;
+			case 'i': nIterations = atoi(optarg); break;
 			case 't': thresh = atof(optarg); break;
 			case 'c':
 				clamp_lo = 0;
@@ -105,6 +141,7 @@ int main(int argc, char **argv) {
 				switch (*optarg) {
 					case 'L': algo = Algo::LogLin; if (verbose) cout << "LogLin algorithm selected." << endl; break;
 					case 'A': algo = Algo::ARLO; if (verbose) cout << "ARLO algorithm selected." << endl; break;
+					case 'N': algo = Algo::Nonlin; if (verbose) cout << "Non-linear algorithm (Levenberg Marquardt) selected." << endl; break;
 					default:
 						cout << "Unknown algorithm type " << optarg << endl;
 						return EXIT_FAILURE;
@@ -128,31 +165,17 @@ int main(int argc, char **argv) {
 	inputFile.open(argv[optind], Nifti::Mode::Read);
 	checkHeaders(inputFile.header(), {maskFile});
 	Agilent::ProcPar pp; ReadPP(inputFile, pp);
-	double TE1, ESP;
-	if (pp) {
-		TE1 = pp.realValue("te");
-		ESP = pp.realValue("te2");
-		NE = static_cast<int>(pp.realValue("ne"));
-	} else {
-		if (prompt) cout << "Enter first echo-time: " << flush;
-		QUIT::Read<double>::FromLine(cin, TE1);
-		if (prompt) cout << "Enter echo spacing: " << flush;
-		QUIT::Read<double>::FromLine(cin, ESP);
-		if (prompt) cout << "Enter number of echos: " << flush;
-		QUIT::Read<int>::FromLine(cin, NE);
-	}
+	MultiEcho multiecho(prompt, pp);
 	// Check that NE makes sense
+	int NE = multiecho.size();
 	if ((inputFile.dim(4) % NE) != 0) {
 		throw(runtime_error("Number of volumes is not a multiple of NE."));
 	}
 	int NVols = inputFile.dim(4) / NE;
 
 	// Set up echo times array
-	MatrixXd X(NE, 2);
-	X(0, 0) = TE1;
-	for (int i = 1; i < X.rows(); i++) {
-		X(i, 0) = X(i-1, 0) + ESP;
-	}
+	MatrixXd X(multiecho.size(), 2);
+	X.col(0) = multiecho.m_TE;
 	X.col(1).setOnes();
 
 	if (verbose) {
@@ -169,7 +192,7 @@ int main(int argc, char **argv) {
 	// Do the fitting
 	//**************************************************************************
 	const auto dims = inputFile.matrix();
-	MultiArray<float, 4> T2starVol(dims, NVols), PDVol(dims, NVols), ResVol(dims, NVols);
+	MultiArray<float, 4> T2Vol(dims, NVols), PDVol(dims, NVols), ResVol(dims, NVols);
 	MultiArray<float, 4> ResidsVols;
 	if (all_residuals) {
 		ResidsVols = MultiArray<float, 4>(dims, inputFile.dim(4));
@@ -183,40 +206,52 @@ int main(int argc, char **argv) {
 			if (!maskFile || (maskVol[{i,j,k}])) {
 				voxCount++;
 				for (size_t outVol = 0; outVol < NVols; outVol++) {
-					double T2star = 0, PD = 0;
+					double T2 = 0, PD = 0;
 					ArrayXd signal = inputVols.slice<1>({i,j,k,outVol*NE},{0,0,0,NE}).asArray().abs().cast<double>();
 					switch (algo) {
 					case Algo::LogLin: {
 						VectorXd Y = signal.log();
 						VectorXd b = (X.transpose() * X).partialPivLu().solve(X.transpose() * Y);
-						T2star = -1 / b[0];
+						T2 = -1 / b[0];
 						PD = exp(b[1]);
 					} break;
 					case Algo::ARLO: {
 						double si2sum = 0, sidisum = 0;
 						for (int i = 0; i < NE - 2; i++) {
-							double si = (ESP / 3) * (signal(i) + 4*signal(i+1) + signal(i+2));
+							double si = (multiecho.m_ESP / 3) * (signal(i) + 4*signal(i+1) + signal(i+2));
 							double di = signal(i) - signal(i+2);
 							si2sum += si*si;
 							sidisum = si*di;
 						}
-						T2star = (si2sum + (ESP/3)*sidisum) / ((ESP/3)*si2sum + sidisum);
-						PD = (signal / (-X.col(0).array() / T2star).exp()).mean();
+						T2 = (si2sum + (multiecho.m_ESP/3)*sidisum) / ((multiecho.m_ESP/3)*si2sum + sidisum);
+						PD = (signal / (-X.col(0).array() / T2).exp()).mean();
 					} break;
+					case Algo::Nonlin: {
+						RelaxFunctor f(multiecho, signal, 1, true);
+						NumericalDiff<RelaxFunctor> nDiff(f);
+						LevenbergMarquardt<NumericalDiff<RelaxFunctor>> lm(nDiff);
+						lm.setMaxfev(250);
+						VectorXd p(4);
+						// Don't need T1 of f0 for this (yet)
+						// Basic guess of T2=50ms
+						p << signal(0), 0., 0.05, 0.;
+						lm.lmder1(p);
+						PD = p(0); T2 = p(2);
+					}
 					}
 
 					if (PD < thresh) {
 						PD = 0.;
-						T2star = 0.;
+						T2 = 0.;
 					}
-					T2star = clamp(T2star, clamp_lo, clamp_hi);
-					ArrayXd theory = PD * (-X.col(0).array() / T2star).exp();
+					T2 = clamp(T2, clamp_lo, clamp_hi);
+					ArrayXd theory = PD * (-X.col(0).array() / T2).exp();
 					ArrayXd resids = (signal - theory);
 					if (all_residuals) {
 						ResidsVols.slice<1>({i,j,k,outVol*NE},{0,0,0,NE}).asArray() = resids.cast<float>();
 					}
 					const MultiArray<float, 4>::Index idx{i,j,k,outVol};
-					T2starVol[idx]  = static_cast<float>(T2star);
+					T2Vol[idx]  = static_cast<float>(T2);
 					PDVol[idx]  = static_cast<float>(PD);
 					ResVol[idx] = static_cast<float>(sqrt(resids.square().sum() / resids.rows()) / PD);
 				}
@@ -244,7 +279,7 @@ int main(int argc, char **argv) {
 	outHdr.intent = Nifti::Intent::Estimate;
 	outHdr.intent_name = "T2* (seconds)";
 	Nifti::File outFile(outHdr, outPrefix + "T2" + suffix + OutExt());
-	outFile.writeVolumes(T2starVol.begin(), T2starVol.end());
+	outFile.writeVolumes(T2Vol.begin(), T2Vol.end());
 	outFile.close();
 	outHdr.intent_name = "PD (au)";
 	outFile.setHeader(outHdr);
